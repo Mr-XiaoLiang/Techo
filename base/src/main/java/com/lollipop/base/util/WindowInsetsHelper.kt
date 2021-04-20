9de37b94376be8d685cdb9ba673ac5ff4952f710
Lollipop <liujian594@hotmail.com>
2021-04-20 22:52:36 +0800

    增加缩进辅助工具

--- base/src/main/java/com/lollipop/base/util/WindowInsetsHelper.kt
+++ base/src/main/java/com/lollipop/base/util/WindowInsetsHelper.kt
@@ -2,6 +2,7 @@ package com.lollipop.base.util
 
 import android.graphics.Rect
 import android.view.View
+import android.view.ViewGroup
 import java.lang.ref.WeakReference
 
 /**
@@ -15,6 +16,47 @@ class WindowInsetsHelper {
 
     fun bind(target: View) {
         targetView = WeakReference<View>(target)
+    }
+
+    fun updateByPadding(rootView: View, left: Int, top: Int, right: Int, bottom: Int) {
+        updateByType(OptionType.Padding, rootView, left, top, right, bottom)
+    }
+
+    fun updateByMargin(rootView: View, left: Int, top: Int, right: Int, bottom: Int) {
+        updateByType(OptionType.Margin, rootView, left, top, right, bottom)
+    }
+
+    fun setMargin(left: Int, top: Int, right: Int, bottom: Int) {
+        targetView.get()?.let { target ->
+            target.layoutParams?.let { layoutParams ->
+                if (layoutParams is ViewGroup.MarginLayoutParams) {
+                    layoutParams.setMargins(left, top, right, bottom)
+                }
+                target.layoutParams = layoutParams
+            }
+        }
+    }
+
+    fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
+        targetView.get()?.setPadding(left, top, right, bottom)
+    }
+
+    private fun updateByType(
+            type: OptionType,
+            rootView: View,
+            left: Int,
+            top: Int,
+            right: Int,
+            bottom: Int) {
+        val target = targetView.get()?:return
+        if (!target.isAttachedToWindow) {
+            return
+        }
+
+    }
+
+    private fun postOption(option: Option) {
+        // TODO
     }
 
     /**
