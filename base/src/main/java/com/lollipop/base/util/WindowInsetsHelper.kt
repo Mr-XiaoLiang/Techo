88ecb271381cc0ede6e9a0cf57337cb9f2c655e5
Lollipop <liujian594@hotmail.com>
2021-04-23 21:46:52 +0800

    增加缩进辅助工具

--- base/src/main/java/com/lollipop/base/util/WindowInsetsHelper.kt
+++ base/src/main/java/com/lollipop/base/util/WindowInsetsHelper.kt
@@ -3,7 +3,9 @@ package com.lollipop.base.util
 import android.graphics.Rect
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewParent
 import java.lang.ref.WeakReference
+import kotlin.math.max
 
 /**
  * @author lollipop
@@ -21,6 +23,24 @@ class WindowInsetsHelper {
      * 计算时，会判定是否真的位于ViewPager，并且计算偏移量
      */
     var targetInViewPager = false
+
+    /**
+     * 一个被复用的尺寸结构体
+     * 主要用于计算位置的偏差
+     */
+    private val tempRectByLocation = Rect()
+
+    /**
+     * 基础的Margin值，它表示了最小的margin
+     * 如果缩进尺寸小于此值，那么会保持在此值
+     */
+    private val baseMargin = Rect()
+
+    /**
+     * 基础的padding值，它代表了最小的padding值
+     * 如果缩进尺寸小于此值，那么将会保持在此值
+     */
+    private val basePadding = Rect()
 
     /**
      * 绑定Target目标View
@@ -70,12 +90,7 @@ class WindowInsetsHelper {
      */
     fun setMargin(left: Int, top: Int, right: Int, bottom: Int) {
         targetView.get()?.let { target ->
-            target.layoutParams?.let { layoutParams ->
-                if (layoutParams is ViewGroup.MarginLayoutParams) {
-                    layoutParams.setMargins(left, top, right, bottom)
-                }
-                target.layoutParams = layoutParams
-            }
+            setMargin(target, left, top, right, bottom)
         }
     }
 
@@ -87,7 +102,22 @@ class WindowInsetsHelper {
      * @param bottom 底部缩进
      */
     fun setPadding(left: Int, top: Int, right: Int, bottom: Int) {
-        targetView.get()?.setPadding(left, top, right, bottom)
+        targetView.get()?.let { target ->
+            setPadding(target, left, top, right, bottom)
+        }
+    }
+
+    private fun setMargin(target: View, left: Int, top: Int, right: Int, bottom: Int) {
+        target.layoutParams?.let { layoutParams ->
+            if (layoutParams is ViewGroup.MarginLayoutParams) {
+                layoutParams.setMargins(left, top, right, bottom)
+            }
+            target.layoutParams = layoutParams
+        }
+    }
+
+    private fun setPadding(target: View, left: Int, top: Int, right: Int, bottom: Int) {
+        target.setPadding(left, top, right, bottom)
     }
 
     private fun updateByType(
@@ -98,7 +128,7 @@ class WindowInsetsHelper {
             right: Int,
             bottom: Int) {
         val target = targetView.get() ?: return
-        if (!target.isAttachedToWindow) {
+        if (!target.isAttachedToWindow || target.width < 1 || target.height < 1) {
             postOption(Option(
                     type,
                     WeakReference(rootView),
@@ -106,8 +136,61 @@ class WindowInsetsHelper {
             ))
             return
         }
+        // 如果不是自己的父节点，那么放弃事件，因为他的事件毫无意义
+        if (!checkRoot(rootView, target)) {
+            return
+        }
+        val offsetByRoot = findOffsetByRoot(rootView, target)
+        when (type) {
+            OptionType.Padding -> {
+                getDiff(offsetByRoot, basePadding) { l, t, r, b ->
+                    setPadding(target, l, t, r, b)
+                }
+            }
+            OptionType.Margin -> {
+                getDiff(offsetByRoot, baseMargin) { l, t, r, b ->
+                    setMargin(target, l, t, r, b)
+                }
+            }
+        }
+    }
 
-        // TODO
+    private fun getDiff(targetInsets: Rect, minInsets: Rect,
+                        callback: (l: Int, t: Int, r: Int, b: Int) -> Unit) {
+        callback(
+                max(targetInsets.left, minInsets.left),
+                max(targetInsets.top, minInsets.top),
+                max(targetInsets.right, minInsets.right),
+                max(targetInsets.bottom, minInsets.bottom)
+        )
+    }
+
+    private fun findOffsetByRoot(rootView: View, target: View): Rect {
+        val rootLocation = rootView.getLocationInWindow()
+        val targetLocation = target.getLocationInWindow()
+        val offsetLeft = targetLocation[0] - rootLocation[0]
+        val offsetTop = targetLocation[1] - rootLocation[1]
+        val offsetRight = rootLocation[0] + rootView.width - targetLocation[0] - target.width
+        val offsetBottom = rootLocation[1] + rootView.height - targetLocation[1] - target.height
+        tempRectByLocation.set(offsetLeft, offsetTop, offsetRight, offsetBottom)
+        return tempRectByLocation
+    }
+
+    private fun View.getLocationInWindow(): IntArray {
+        val location = IntArray(2)
+        getLocationInWindow(location)
+        return location
+    }
+
+    private fun checkRoot(rootView: View, target: View): Boolean {
+        var parent: ViewParent? = target.parent
+        while (parent != null) {
+            if (parent == rootView) {
+                return true
+            }
+            parent = parent.parent
+        }
+        return false
     }
 
     private fun postOption(option: Option) {
