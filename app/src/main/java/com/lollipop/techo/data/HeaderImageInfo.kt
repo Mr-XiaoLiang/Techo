af6c12fa69e557751d08b86666c2fa7a5347f430
Lollipop <liujian594@hotmail.com>
2021-05-01 22:41:32 +0800

    增加Json处理的Bean

---
+++ app/src/main/java/com/lollipop/techo/data/HeaderImageInfo.kt
@@ -0,0 +1,11 @@
+package com.lollipop.techo.data
+
+import com.lollipop.techo.data.json.JsonObjectInfo
+
+/**
+ * @author lollipop
+ * @date 5/1/21 22:40
+ * 顶部图片的数据结构体
+ */
+class HeaderImageInfo: JsonObjectInfo() {
+}
