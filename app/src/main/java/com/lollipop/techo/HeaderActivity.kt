28fac6336eb83e827d415bd7db1328b3ea68a345
Lollipop <liujian594@hotmail.com>
2021-05-06 20:20:43 +0800

    base页面增加顶部图片的加载方法

--- app/src/main/java/com/lollipop/techo/HeaderActivity.kt
+++ app/src/main/java/com/lollipop/techo/HeaderActivity.kt
@@ -3,10 +3,15 @@ package com.lollipop.techo
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.lollipop.base.ui.BaseActivity
+import com.lollipop.base.util.doAsync
 import com.lollipop.base.util.lazyBind
 import com.lollipop.techo.data.HeaderImageInfo
+import com.lollipop.techo.data.RequestService
 import com.lollipop.techo.databinding.ActivityHeaderBinding
+import com.lollipop.techo.util.BlurTransformation
 
 /**
  * 带有头部View的Activity
@@ -25,14 +30,38 @@ abstract class HeaderActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
         setSupportActionBar(viewBinding.toolbar)
-        viewBinding.contentRoot.addView(contentView,
-            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        viewBinding.contentRoot.addView(
+            contentView,
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+        )
         floatingView?.let {
-            viewBinding.floatingRoot.addView(it,
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            viewBinding.floatingRoot.addView(
+                it,
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+            )
         }
+        loadHeader()
     }
 
-
+    private fun loadHeader() {
+        doAsync {
+            RequestService.getHeaderImageInfo().let { headerImageInfo ->
+                headerImageInfo.images?.let { images ->
+                    if (images.size > 0) {
+                        images.get<HeaderImageInfo.ImageInfo>(0)?.let { imageInfo ->
+                            if (imageInfo.url.isNotEmpty()) {
+                                Glide.with(viewBinding.headerBackground)
+                                    .load(imageInfo.fullUrl)
+                                    .apply(
+                                        RequestOptions().transform(
+                                            BlurTransformation.create(this@HeaderActivity)))
+                                    .into(viewBinding.headerBackground)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
