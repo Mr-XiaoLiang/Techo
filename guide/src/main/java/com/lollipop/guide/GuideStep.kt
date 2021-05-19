f3b149127e1155abbc5234475f71c97d8f90e726
Lollipop <liujian594@hotmail.com>
2021-05-19 22:07:03 +0800

    添加用户引导工具

---
+++ guide/src/main/java/com/lollipop/guide/GuideStep.kt
@@ -0,0 +1,12 @@
+package com.lollipop.guide
+
+import android.view.View
+
+/**
+ * @author lollipop
+ * @date 2021/5/19 22:06
+ */
+open class GuideStep(
+    val target: View,
+    val info: String
+    )
