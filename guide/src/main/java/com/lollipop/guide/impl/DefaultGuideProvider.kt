73aa41ffb2d33c9eca016edf1b5296e349ee8f49
Lollipop <liujian594@hotmail.com>
2021-06-10 23:16:33 +0800

    更新引导工具的默认实现类

--- guide/src/main/java/com/lollipop/guide/impl/DefaultGuideProvider.kt
+++ guide/src/main/java/com/lollipop/guide/impl/DefaultGuideProvider.kt
@@ -1,7 +1,9 @@
 package com.lollipop.guide.impl
 
 import android.content.Context
+import android.graphics.Color
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.lollipop.guide.GuideProvider
 import com.lollipop.guide.GuideStep
 
@@ -11,6 +13,20 @@ import com.lollipop.guide.GuideStep
  * 默认的引导提供者
  */
 class DefaultGuideProvider: GuideProvider() {
+
+    companion object {
+        val DEFAULT_GUIDE_STYLE = GuideStyle(
+            backgroundColor = Color.BLACK and 0xFFFFFF or 0xAA000000.toInt(),
+            cornerDp = 10,
+            marginVerticalDp = 10,
+            marginHorizontalDp = 10,
+            paddingVerticalDp = 10,
+            paddingHorizontalDp = 10,
+            spaceToTargetDp = 10
+        )
+
+        var guideStyle = DEFAULT_GUIDE_STYLE
+    }
 
     override fun support(step: GuideStep): Boolean {
         return true
@@ -24,10 +40,20 @@ class DefaultGuideProvider: GuideProvider() {
         TODO("Not yet implemented")
     }
 
-    private class GuideView(context: Context): ViewGroup(context) {
+    private class GuideView(context: Context): FrameLayout(context) {
         override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
             TODO("Not yet implemented")
         }
     }
+
+    data class GuideStyle(
+        val backgroundColor: Int,
+        val cornerDp: Int,
+        val marginVerticalDp: Int,
+        val marginHorizontalDp: Int,
+        val paddingVerticalDp: Int,
+        val paddingHorizontalDp: Int,
+        val spaceToTargetDp: Int
+    )
 
 }
