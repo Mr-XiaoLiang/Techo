0c65f5d50ce52b6b5e4b9267f28e2316938277e4
Lollipop <liujian594@hotmail.com>
2021-06-13 21:57:19 +0800

    更新引导工具的默认实现类

--- guide/src/main/java/com/lollipop/guide/impl/DefaultGuideProvider.kt
+++ guide/src/main/java/com/lollipop/guide/impl/DefaultGuideProvider.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.os.Build
+import android.util.TypedValue
+import android.view.Gravity
 import android.widget.FrameLayout
+import android.widget.TextView
 import com.lollipop.guide.GuideProvider
 import com.lollipop.guide.GuideStep
 
@@ -18,12 +21,15 @@ class DefaultGuideProvider : GuideProvider() {
     companion object {
         val DEFAULT_GUIDE_STYLE = GuideStyle(
             backgroundColor = Color.BLACK and 0xFFFFFF or 0xAA000000.toInt(),
+            popColor = Color.BLUE,
+            popTextColor = Color.WHITE,
             cornerDp = 10,
             marginVerticalDp = 10,
             marginHorizontalDp = 10,
             paddingVerticalDp = 10,
             paddingHorizontalDp = 10,
-            spaceToTargetDp = 10
+            spaceToTargetDp = 10,
+            textSizeSp = 16F
         )
 
         var guideStyle = DEFAULT_GUIDE_STYLE
@@ -48,8 +54,14 @@ class DefaultGuideProvider : GuideProvider() {
     private class GuideView(context: Context) : FrameLayout(context) {
 
         private val clipOutDrawable = ClipOutDrawable()
+        private val popBackground = PopDrawable()
 
-        var corner: Float
+        private val popTextView = TextView(context).apply {
+            gravity = Gravity.CENTER
+            background = popBackground
+        }
+
+        var guideClipCorner: Float
             set(value) {
                 clipOutDrawable.corner = value
             }
@@ -57,7 +69,7 @@ class DefaultGuideProvider : GuideProvider() {
                 return clipOutDrawable.corner
             }
 
-        var isOval: Boolean
+        var guideClipIsOval: Boolean
             set(value) {
                 clipOutDrawable.isOval = value
             }
@@ -65,7 +77,7 @@ class DefaultGuideProvider : GuideProvider() {
                 return clipOutDrawable.isOval
             }
 
-        var color: Int
+        var guideBackgroundColor: Int
             set(value) {
                 clipOutDrawable.color = value
             }
@@ -73,12 +85,46 @@ class DefaultGuideProvider : GuideProvider() {
                 return clipOutDrawable.color
             }
 
-        fun changeTargetBounds(left: Int, top: Int, right: Int, bottom: Int) {
-            clipOutDrawable.changeTargetBounds(left, top, right, bottom)
+        var guidePopColor: Int
+            set(value) {
+                popBackground.color = value
+            }
+            get() {
+                return popBackground.color
+            }
+
+        var guidePopTextColor: Int
+            set(value) {
+                popTextView.setTextColor(value)
+            }
+            get() {
+                return popTextView.textColors.defaultColor
+            }
+
+        var guidePopTextSize: Float
+            set(value) {
+                popTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, value)
+            }
+            get() {
+                return popTextView.textSize
+            }
+
+        fun setPopPadding(left: Int, top: Int, right: Int, bottom: Int) {
+            popTextView.setPadding(left, top, right, bottom)
         }
 
         init {
             background = clipOutDrawable
+            addView(popTextView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        }
+
+        fun changeTargetBounds(left: Int, top: Int, right: Int, bottom: Int) {
+            clipOutDrawable.changeTargetBounds(left, top, right, bottom)
+        }
+
+        fun onStepChanged(info: CharSequence) {
+            popTextView.text = info
+            requestLayout()
         }
 
         override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
@@ -181,14 +227,58 @@ class DefaultGuideProvider : GuideProvider() {
 
     }
 
+    private class PopDrawable : Drawable() {
+
+        var corner = 0F
+            set(value) {
+                field = value
+                needUpdatePath = true
+            }
+
+        private var needUpdatePath = true
+
+        private val paint = Paint().apply {
+            isAntiAlias = true
+            isDither = true
+        }
+
+        var color: Int
+            get() {
+                return paint.color
+            }
+            set(value) {
+                paint.color = value
+            }
+
+        override fun draw(canvas: Canvas) {
+            canvas.drawRect(bounds, paint)
+        }
+
+        override fun setAlpha(alpha: Int) {
+            paint.alpha = alpha
+        }
+
+        override fun setColorFilter(colorFilter: ColorFilter?) {
+            paint.colorFilter = colorFilter
+        }
+
+        override fun getOpacity(): Int {
+            return PixelFormat.TRANSPARENT
+        }
+
+    }
+
     data class GuideStyle(
         val backgroundColor: Int,
+        val popColor: Int,
+        val popTextColor: Int,
         val cornerDp: Int,
         val marginVerticalDp: Int,
         val marginHorizontalDp: Int,
         val paddingVerticalDp: Int,
         val paddingHorizontalDp: Int,
-        val spaceToTargetDp: Int
+        val spaceToTargetDp: Int,
+        val textSizeSp: Float
     )
 
 }
