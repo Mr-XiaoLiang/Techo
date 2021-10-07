0bd692babde2eb42b3e57c8d667431751de37f78
Lollipop <liujian594@hotmail.com>
2021-10-07 22:27:28 +0800

    调整手帐数据结构体

--- app/src/main/java/com/lollipop/techo/data/TechoInfo.kt
+++ app/src/main/java/com/lollipop/techo/data/TechoInfo.kt
@@ -1,16 +1,93 @@
 package com.lollipop.techo.data
 
 import android.graphics.Color
-import com.lollipop.techo.data.json.JsonObjectInfo
+import com.lollipop.gallery.Photo
+import com.lollipop.gallery.PhotoGridLayout
 
 /**
  * @author lollipop
  * @date 4/29/21 22:04
  */
-class TechoInfo: JsonObjectInfo() {
+data class TechoInfo(
+    var title: String = "",
+    var color: Int = Color.TRANSPARENT,
+    val items: MutableList<BaseTechoItem> = mutableListOf()
+)
 
-    var title by withThis("")
+enum class TechoItemType {
+    Empty,
+    Text,
+    Number,
+    CheckBox,
+    Photo,
+    Split;
 
-    var color by withThis(Color.TRANSPARENT)
+    companion object {
+        fun pauseFromOrdinal(ordinal: Int): TechoItemType {
+            val valueArray = values()
+            if (ordinal in valueArray.indices) {
+                return valueArray[ordinal]
+            }
+            return valueArray[0]
+        }
+    }
+}
+
+object FontStyle {
+
+    /**
+     * 什么都没有
+     */
+    const val NORMAL = 0
+
+    /**
+     * 加粗
+     */
+    const val BOLD = 1
+
+    /**
+     * 斜体
+     */
+    const val ITALIC = 1 shl 1
+
+    /**
+     * 删除线
+     */
+    const val STRIKETHROUGH = 1 shl 2
+
+    fun has(value: Int, flag: Int): Boolean {
+        return value and flag != 0
+    }
 
 }
+
+open class BaseTechoItem {
+
+}
+
+open class EmptyItem : BaseTechoItem()
+
+open class TextItem(
+    val values: MutableList<TextSpan> = mutableListOf()
+) : BaseTechoItem()
+
+open class TextSpan(
+    var text: String = "",
+    var color: Int = Color.BLACK,
+    var style: Int
+)
+
+open class NumberItem(
+    var number: Int = 0
+) : TextItem()
+
+open class CheckBoxItem(
+    var isChecked: Boolean = false
+) : TextItem()
+
+class SplitItem : EmptyItem()
+
+class PhotoItem(
+    val values: MutableList<Photo> = mutableListOf(),
+    val style: PhotoGridLayout.Style = PhotoGridLayout.Style.Playbill
+): BaseTechoItem()
