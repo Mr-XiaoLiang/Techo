fc4e62708921bdeea46af5e13c414acd27ac65a3
Lollipop <liujian594@hotmail.com>
2021-10-08 21:03:52 +0800

    增加整体的数据转入转出接口

--- app/src/main/java/com/lollipop/techo/data/TechoInfo.kt
+++ app/src/main/java/com/lollipop/techo/data/TechoInfo.kt
@@ -3,6 +3,9 @@ package com.lollipop.techo.data
 import android.graphics.Color
 import com.lollipop.gallery.Photo
 import com.lollipop.gallery.PhotoGridLayout
+import com.lollipop.techo.data.TechoItemType.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * @author lollipop
@@ -12,7 +15,81 @@ data class TechoInfo(
     var title: String = "",
     var flag: TechoFlag = TechoFlag(),
     val items: MutableList<BaseTechoItem> = mutableListOf()
-)
+) {
+
+    companion object {
+
+        private const val KEY_TITLE = "title"
+        private const val KEY_FLAG_ID = "flagId"
+        private const val KEY_FLAG_COLOR = "flagColor"
+        private const val KEY_FLAG_NAME = "flagName"
+        private const val KEY_ITEMS = "items"
+
+        fun fromJson(json: String): TechoInfo {
+            val info = TechoInfo()
+            try {
+                val jsonObject = JSONObject(json)
+                info.title = jsonObject.optString(KEY_TITLE)
+                info.flag.id = jsonObject.optInt(KEY_FLAG_ID, 0)
+                info.flag.name = jsonObject.optString(KEY_FLAG_NAME)
+                info.flag.color = jsonObject.optInt(KEY_FLAG_COLOR, Color.BLACK)
+                val optJSONArray = jsonObject.optJSONArray(KEY_ITEMS)
+                if (optJSONArray != null) {
+                    for (index in 0 until optJSONArray.length()) {
+                        val itemObj = optJSONArray.optJSONObject(index)
+                        val itemInfo = getItemByType(BaseTechoItem.getType(itemObj))
+                        itemInfo.parse(itemObj)
+                        info.items.add(itemInfo)
+                    }
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
+            return info
+        }
+
+        private fun getItemByType(type: TechoItemType): BaseTechoItem {
+            return when (type) {
+                Empty -> {
+                    EmptyItem()
+                }
+                Text -> {
+                    TextItem()
+                }
+                Number -> {
+                    NumberItem()
+                }
+                CheckBox -> {
+                    CheckBoxItem()
+                }
+                Photo -> {
+                    PhotoItem()
+                }
+                Split -> {
+                    SplitItem()
+                }
+            }
+        }
+
+        private fun toJson(info: TechoInfo): JSONObject {
+            val jsonObject = JSONObject()
+            jsonObject.put(KEY_TITLE, info.title)
+            jsonObject.put(KEY_FLAG_ID, info.flag.id)
+            jsonObject.put(KEY_FLAG_NAME, info.flag.name)
+            jsonObject.put(KEY_FLAG_COLOR, info.flag.color)
+            val itemArray = JSONArray()
+            info.items.forEach {
+                itemArray.put(it.toJson())
+            }
+            jsonObject.put(KEY_ITEMS, itemArray)
+            return jsonObject
+        }
+    }
+
+    fun toJson(): JSONObject {
+        return toJson(this)
+    }
+}
 
 data class TechoFlag(
     var name: String = "",
@@ -67,15 +144,40 @@ object FontStyle {
 
 }
 
-open class BaseTechoItem {
+abstract class BaseTechoItem() {
+
+    companion object {
+        private const val KEY_ITEM_TYPE = "itemType"
+
+        fun getType(json: JSONObject): TechoItemType {
+            return TechoItemType.pauseFromOrdinal(json.optInt(KEY_ITEM_TYPE))
+        }
+
+    }
+
+    abstract val itemType: TechoItemType
+
+    open fun toJson(): JSONObject {
+        return JSONObject().apply {
+            put(KEY_ITEM_TYPE, itemType.ordinal)
+        }
+    }
+
+    open fun parse(json: JSONObject) {
+
+    }
 
 }
 
-open class EmptyItem : BaseTechoItem()
+open class EmptyItem : BaseTechoItem() {
+    override val itemType: TechoItemType = Empty
+}
 
 open class TextItem(
     val values: MutableList<TextSpan> = mutableListOf()
-) : BaseTechoItem()
+) : BaseTechoItem() {
+    override val itemType: TechoItemType = Empty
+}
 
 open class TextSpan(
     var text: String = "",
@@ -85,15 +187,23 @@ open class TextSpan(
 
 open class NumberItem(
     var number: Int = 0
-) : TextItem()
+) : TextItem() {
+    override val itemType: TechoItemType = Number
+}
 
 open class CheckBoxItem(
     var isChecked: Boolean = false
-) : TextItem()
+) : TextItem() {
+    override val itemType: TechoItemType = CheckBox
+}
 
-class SplitItem : EmptyItem()
+class SplitItem : EmptyItem() {
+    override val itemType: TechoItemType = Split
+}
 
 class PhotoItem(
     val values: MutableList<Photo> = mutableListOf(),
     val style: PhotoGridLayout.Style = PhotoGridLayout.Style.Playbill
-): BaseTechoItem()
+): BaseTechoItem() {
+    override val itemType: TechoItemType = Photo
+}
