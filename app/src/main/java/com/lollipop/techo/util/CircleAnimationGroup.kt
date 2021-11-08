9d5ca9442121e318650cb73a5500d36330548277
Lollipop <liujian594@hotmail.com>
2021-11-08 19:03:22 +0800

    增加卫星展开动画效果

--- app/src/main/java/com/lollipop/techo/util/CircleAnimationGroup.kt
+++ app/src/main/java/com/lollipop/techo/util/CircleAnimationGroup.kt
@@ -1,16 +1,38 @@
 package com.lollipop.techo.util
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.view.View
+import androidx.core.view.isInvisible
+import kotlin.math.abs
 
 /**
  * @author lollipop
  * @date 2021/10/19 22:39
  * 圆形的展开动画管理组
  */
-class CircleAnimationGroup {
+class CircleAnimationGroup : Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
+
+    companion object {
+        private const val MIN = 0F
+        private const val MAX = 1F
+        private const val CLOSED = 0.001F
+        private const val OPENED = 0.999F
+        private const val DURATION = 300L
+        private const val LENGTH = MAX - MIN
+    }
 
     private val planetViewList = ArrayList<View>()
     private var centerView: View? = null
+
+    private var animationProgress = 0F
+
+    private val valueAnimator by lazy {
+        ValueAnimator().apply {
+            addListener(this@CircleAnimationGroup)
+            addUpdateListener(this@CircleAnimationGroup)
+        }
+    }
 
     fun addPlanet(vararg views: View) {
         planetViewList.addAll(views)
@@ -21,26 +43,93 @@ class CircleAnimationGroup {
     }
 
     fun close() {
-        // TODO 动画的关闭
+        centerView?.post {
+            valueAnimator.cancel()
+            valueAnimator.duration = (abs(animationProgress - MIN) / LENGTH * DURATION).toLong()
+            valueAnimator.setFloatValues(animationProgress, MIN)
+            valueAnimator.start()
+        }
     }
 
     fun open() {
-        // TODO 动画的展开
+        centerView?.post {
+            valueAnimator.cancel()
+            valueAnimator.duration = (abs(animationProgress - MAX) / LENGTH * DURATION).toLong()
+            valueAnimator.setFloatValues(animationProgress, MAX)
+            valueAnimator.start()
+        }
     }
 
     fun hide() {
-        // TODO 静态的隐藏
+        valueAnimator.cancel()
+        animationProgress = MIN
+        update()
+        visibleChange(true)
     }
 
     fun show() {
-        // TODO 静态的显示
+        valueAnimator.cancel()
+        animationProgress = MAX
+        update()
+        visibleChange(false)
     }
 
     fun destroy() {
-        // TODO 停止动画
-
+        valueAnimator.cancel()
         planetViewList.clear()
         centerView = null
+    }
+
+    private fun visibleChange(invisible: Boolean) {
+        planetViewList.forEach {
+            it.isInvisible = invisible
+        }
+    }
+
+    private fun update() {
+        val progress = animationProgress
+        val center = centerView ?: return
+        val centerX = half(center.left, center.right)
+        val centerY = half(center.top, center.bottom)
+        planetViewList.forEach {
+            updateView(it, centerX, centerY, progress)
+        }
+    }
+
+    private fun updateView(planet: View, centerX: Int, centerY: Int, progress: Float) {
+        val planetX = half(planet.left, planet.right)
+        val planetY = half(planet.top, planet.bottom)
+        planet.translationX = (centerX - planetX) * progress
+        planet.translationY = (centerY - planetY) * progress
+    }
+
+    private fun half(value1: Int, value2: Int): Int {
+        return (value1 + value2) / 2
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+        if (animation == valueAnimator) {
+            visibleChange(false)
+        }
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+        if (animation == valueAnimator && animationProgress >= CLOSED) {
+            visibleChange(true)
+        }
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+    }
+
+    override fun onAnimationRepeat(animation: Animator?) {
+    }
+
+    override fun onAnimationUpdate(animation: ValueAnimator?) {
+        if (animation == valueAnimator) {
+            animationProgress = animation.animatedValue as Float
+            update()
+        }
     }
 
 }
