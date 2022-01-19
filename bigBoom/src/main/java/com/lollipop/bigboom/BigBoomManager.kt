99eaa9a1340ac5633f715fb22d571c01f280efbf
Lollipop <liujian594@hotmail.com>
2022-01-19 23:37:38 +0800

    增加大爆炸的管理类

---
+++ bigBoom/src/main/java/com/lollipop/bigboom/BigBoomManager.kt
@@ -0,0 +1,54 @@
+package com.lollipop.bigboom
+
+/**
+ * 大爆炸的辅助工具类
+ * 它会绑定一个展示的View
+ * 然后将炸药交给他后，将会自动把碎片交给展示的View
+ */
+class BigBoomManager(
+    val boomView: BigBoomViewProvider
+) : Basket {
+
+    var valueArray = emptyArray<String>()
+        private set
+    private var onErrorListener: OnErrorListener? = null
+    private var onValuesChangedCallback: OnValuesChangedCallback? = null
+
+    private var currentExplosive: Explosive? = null
+
+    fun boom(e: Explosive) {
+        currentExplosive = e
+        e.pilot(this)
+    }
+
+    fun onError(listener: OnErrorListener?) {
+        onErrorListener = listener
+    }
+
+    fun onValuesChanged(callback: OnValuesChangedCallback?) {
+        onValuesChangedCallback = callback
+    }
+
+    override fun goodPatches(value: Array<String>) {
+        valueArray = value
+        val defaultSelected = onValuesChangedCallback?.onValuesChanged(currentExplosive, value)
+        boomView.getBoomView()?.setPatches(value, defaultSelected ?: emptyArray())
+    }
+
+    override fun failedDetonate(code: Int, msg: String, throwable: Throwable?) {
+        onErrorListener?.failedDetonate(code, msg, throwable)
+    }
+
+    fun interface BigBoomViewProvider {
+        fun getBoomView(): BigBoomView?
+    }
+
+    fun interface OnErrorListener {
+        fun failedDetonate(code: Int, msg: String, throwable: Throwable?)
+    }
+
+    fun interface OnValuesChangedCallback {
+        fun onValuesChanged(e: Explosive?, value: Array<String>): Array<Int>
+    }
+
+}
