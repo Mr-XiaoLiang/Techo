08ba7ce4f8ac490b07abeb44391a4aedf1d8ba6e
Lollipop <liujian594@hotmail.com>
2022-01-21 23:20:54 +0800

    增加矩形碎片Item提供者

---
+++ bigBoom/src/main/java/com/lollipop/bigboom/item/RectanglePatchesItemProvider.kt
@@ -0,0 +1,13 @@
+package com.lollipop.bigboom.item
+
+import android.view.ViewGroup
+import com.lollipop.bigboom.PatchesHolder
+import com.lollipop.bigboom.PatchesItemProvider
+
+class RectanglePatchesItemProvider: PatchesItemProvider {
+
+    override fun createHolder(parent: ViewGroup): PatchesHolder {
+        TODO("Not yet implemented")
+    }
+
+}
