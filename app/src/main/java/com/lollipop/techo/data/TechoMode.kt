35397375c7230f4a539edd0d71beedaa74581878
Lollipop <liujian594@hotmail.com>
2022-02-20 23:34:24 +0800

    增加手帐内容的数据处理模块

---
+++ app/src/main/java/com/lollipop/techo/data/TechoMode.kt
@@ -0,0 +1,134 @@
+package com.lollipop.techo.data
+
+import android.content.Context
+import com.lollipop.base.util.doAsync
+import com.lollipop.base.util.onUI
+import java.lang.ref.WeakReference
+
+object TechoMode {
+
+    class Detail(
+        listener: StateListener,
+        context: Context
+    ) : BaseMode(listener) {
+
+        companion object {
+            private const val NO_ID = 0
+        }
+
+        private val dbUtil = TechoDbUtil(context)
+
+        val info = TechoInfo()
+
+        fun reset() {
+            loadStart()
+            info.id = NO_ID
+            info.flag = TechoFlag()
+            info.title = ""
+            info.items.clear()
+            initList()
+            infoChanged(0, info.items.size)
+            loadEnd()
+        }
+
+        fun new() {
+            reset()
+        }
+
+        fun load() {
+            loadStart()
+            if (info.id == NO_ID) {
+                infoChanged(0, info.items.size)
+                loadEnd()
+                return
+            }
+            doAsync {
+                val newInfo = dbUtil.selectTechoById(info.id)
+                if (newInfo != null) {
+                    info.flag = newInfo.flag
+                    info.items.clear()
+                    info.items.addAll(newInfo.items)
+                    info.title = newInfo.title
+                }
+                initList()
+                onUI {
+                    infoChanged(0, info.items.size)
+                    loadEnd()
+                }
+            }
+        }
+
+        private fun initList() {
+            if (info.items.isEmpty()) {
+                info.items.add(TextItem())
+            }
+        }
+
+    }
+
+    class List(
+        listener: StateListener,
+        context: Context
+    ) : BaseMode(listener) {
+
+        companion object {
+            private const val DEFAULT_PAGE_INDEX = 0
+        }
+
+        val info = ArrayList<TechoInfo>()
+
+        private var pageIndex = DEFAULT_PAGE_INDEX
+
+        private val pageSize = 20
+
+        private val dbUtil = TechoDbUtil(context)
+
+        fun loadNext() {
+            loadStart()
+            doAsync {
+                val newList = dbUtil.selectTecho(pageIndex, pageSize)
+                pageIndex++
+                val start = info.size
+                val count = newList.size
+                info.addAll(newList)
+                onUI {
+                    infoChanged(start, count)
+                    loadEnd()
+                }
+            }
+        }
+
+        fun refresh() {
+            pageIndex = DEFAULT_PAGE_INDEX
+            loadNext()
+        }
+
+    }
+
+    open class BaseMode(
+        listener: StateListener
+    ) {
+
+        private val listenerWrapper = WeakReference(listener)
+
+        protected fun loadStart() {
+            listenerWrapper.get()?.onLoadStart()
+        }
+
+        protected fun loadEnd() {
+            listenerWrapper.get()?.onLoadEnd()
+        }
+
+        protected fun infoChanged(start: Int, count: Int) {
+            listenerWrapper.get()?.onInfoChanged(start, count)
+        }
+
+    }
+
+    interface StateListener {
+        fun onLoadStart()
+        fun onLoadEnd()
+        fun onInfoChanged(start: Int, count: Int)
+    }
+
+}
