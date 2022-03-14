9652fb3138049e19788e5057805f627c4d0a30f9
Lollipop <liujian594@hotmail.com>
2022-03-14 21:41:33 +0800

    调整Web实现方案

--- web/src/main/java/com/lollipop/web/IWeb.kt
+++ web/src/main/java/com/lollipop/web/IWeb.kt
@@ -1,11 +1,16 @@
 package com.lollipop.web
 
 import android.view.View
+import com.lollipop.web.bridge.Bridge
 
 interface IWeb {
 
     val host: WebHost
 
     val view: View
+
+    fun addBridge(bridge: Bridge)
+
+    fun load(url: String)
 
 }
--- web/src/main/java/com/lollipop/web/WebHelper.kt
+++ web/src/main/java/com/lollipop/web/WebHelper.kt
@@ -1,6 +1,7 @@
 package com.lollipop.web
 
 import android.view.View
+import com.lollipop.web.bridge.Bridge
 
 /**
  * 放弃继承和包装，采用组合组件的形式来提供Web的支持能力
@@ -8,9 +9,45 @@ import android.view.View
 class WebHelper(private val host: WebHost, private val iWeb: IWeb) {
 
     companion object {
+
+        private val globeBridge = ArrayList<Class<out Bridge>>()
+
         fun bind(host: WebHost, view: View): WebHelper {
             return WebHelper(host, IWebFactory.create(host, view))
         }
+
+        fun register(clazz: Class<out Bridge>) {
+            if (globeBridge.contains(clazz)) {
+                return
+            }
+            globeBridge.add(clazz)
+        }
+
+        fun unregister(clazz: Class<out Bridge>) {
+            globeBridge.remove(clazz)
+        }
+
+    }
+
+    private var isInit = false
+
+    fun addBridge(bridge: Bridge) {
+        iWeb.addBridge(bridge)
+    }
+
+    fun init() {
+        if (isInit) {
+            return
+        }
+        isInit = true
+        globeBridge.forEach {
+            addBridge(it.newInstance())
+        }
+    }
+
+    fun loadUrl(url: String) {
+        init()
+        iWeb.load(url)
     }
 
 }
