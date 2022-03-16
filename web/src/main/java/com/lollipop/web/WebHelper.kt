4ea7a9f318444582b5f021a46a098fb3a50e8c4d
Lollipop <liujian594@hotmail.com>
2022-03-16 17:19:46 +0800

    增加Web库的基础实现

--- web/src/main/java/com/lollipop/web/WebHelper.kt
+++ web/src/main/java/com/lollipop/web/WebHelper.kt
@@ -2,37 +2,69 @@ package com.lollipop.web
 
 import android.view.View
 import com.lollipop.web.bridge.Bridge
+import com.lollipop.web.bridge.BridgeRoot
 
 /**
  * 放弃继承和包装，采用组合组件的形式来提供Web的支持能力
  */
-class WebHelper(private val host: WebHost, private val iWeb: IWeb) {
+class WebHelper(private val iWeb: IWeb) {
 
     companion object {
 
-        private val globeBridge = ArrayList<Class<out Bridge>>()
+        private val globeBridgeRoot = ArrayList<Class<out BridgeRoot>>()
+        private val globeBridge = HashMap<String, ArrayList<Class<out Bridge>>>()
 
         fun bind(host: WebHost, view: View): WebHelper {
-            return WebHelper(host, IWebFactory.create(host, view))
+            return WebHelper(IWebFactory.create(host, view))
         }
 
-        fun register(clazz: Class<out Bridge>) {
-            if (globeBridge.contains(clazz)) {
+        fun register(clazz: Class<out BridgeRoot>) {
+            if (globeBridgeRoot.contains(clazz)) {
                 return
             }
-            globeBridge.add(clazz)
+            globeBridgeRoot.add(clazz)
         }
 
-        fun unregister(clazz: Class<out Bridge>) {
-            globeBridge.remove(clazz)
+        fun register(rootName: String, clazz: Class<out Bridge>) {
+            val bridgeList = globeBridge[rootName] ?: ArrayList()
+            if (bridgeList.contains(clazz)) {
+                return
+            }
+            bridgeList.add(clazz)
+            globeBridge[rootName] = bridgeList
+        }
+
+        fun unregister(clazz: Class<out BridgeRoot>) {
+            globeBridgeRoot.remove(clazz)
+        }
+
+        fun unregister(rootName: String, clazz: Class<out Bridge>) {
+            val bridgeList = globeBridge[rootName] ?: return
+            bridgeList.remove(clazz)
         }
 
     }
 
     private var isInit = false
 
-    fun addBridge(bridge: Bridge) {
-        iWeb.addBridge(bridge)
+    val canRegisterBridgeRoot: Boolean
+        get() {
+            return !isInit
+        }
+
+    private val bridgeRootList = ArrayList<BridgeRoot>()
+
+    fun addBridgeRoot(bridge: BridgeRoot) {
+        iWeb.addBridgeRoot(bridge)
+    }
+
+    fun addBridge(rootName: String, bridge: Bridge) {
+        this.bridgeRootList.forEach {
+            if (it.name == rootName) {
+                it.addBridge(bridge)
+                return
+            }
+        }
     }
 
     fun init() {
@@ -40,8 +72,16 @@ class WebHelper(private val host: WebHost, private val iWeb: IWeb) {
             return
         }
         isInit = true
-        globeBridge.forEach {
-            addBridge(it.newInstance())
+        globeBridgeRoot.forEach {
+            addBridgeRoot(it.newInstance())
+        }
+        globeBridge.forEach { entry ->
+            val key = entry.key
+            this.bridgeRootList.find { it.name == key }?.let { root ->
+                entry.value.forEach { bridge ->
+                    root.addBridge(bridge.newInstance())
+                }
+            }
         }
     }
 
