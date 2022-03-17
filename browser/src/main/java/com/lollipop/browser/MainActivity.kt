4ebbe6bf7b4372a10fda623d0317c4f2c2f2a575
Lollipop <liujian594@hotmail.com>
2022-03-17 17:33:39 +0800

    增加浏览器模块的简单实现

--- browser/src/main/java/com/lollipop/browser/MainActivity.kt
+++ browser/src/main/java/com/lollipop/browser/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.lollipop.browser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lollipop.base.util.lazyBind
+import com.lollipop.browser.databinding.ActivityMainBinding
+import com.lollipop.web.WebHelper
+import com.lollipop.web.host.ActivityHost
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActivityHost {
+
+    private val binding: ActivityMainBinding by lazyBind()
+
+    private val webHelper by lazy {
+        WebHelper.bind(this, binding.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.doneBtn.setOnClickListener {
+            webHelper.loadUrl(binding.urlInput.text?.toString() ?: "")
+        }
     }
 }
