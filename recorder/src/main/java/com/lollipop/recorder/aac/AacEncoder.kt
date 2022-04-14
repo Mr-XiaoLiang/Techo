27532bc6a755aa943d475736a30c8295a910ed88
Lollipop <liujian594@hotmail.com>
2022-04-14 21:04:24 +0800

    推进录音机的简单实现

--- recorder/src/main/java/com/lollipop/recorder/aac/AacEncoder.kt
+++ recorder/src/main/java/com/lollipop/recorder/aac/AacEncoder.kt
@@ -1,10 +1,27 @@
 package com.lollipop.recorder.aac
 
+import android.media.MediaCodec
+import android.media.MediaFormat
 import com.lollipop.recorder.encode.PcmEncoder
 
 class AacEncoder: PcmEncoder() {
 
+    private var mediaCodec: MediaCodec? = null
+
+    override fun onReady() {
+        super.onReady()
+        mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaCodec?.release()
+        mediaCodec = null
+    }
+
     override fun write(byteArray: ByteArray, offset: Int, count: Int) {
+        mediaCodec?:return
         TODO("Not yet implemented")
     }
 
