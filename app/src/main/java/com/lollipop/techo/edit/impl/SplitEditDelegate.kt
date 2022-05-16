67d6f1fa0d929df7dd44b8a406037446d54c0b06
Lollipop <liujian594@hotmail.com>
2022-05-16 20:36:49 +0800

    推进分割线选择器的进度

--- app/src/main/java/com/lollipop/techo/edit/impl/SplitEditDelegate.kt
+++ app/src/main/java/com/lollipop/techo/edit/impl/SplitEditDelegate.kt
@@ -2,21 +2,27 @@ package com.lollipop.techo.edit.impl
 
 import android.view.View
 import android.view.ViewGroup
+import com.lollipop.base.util.bind
 import com.lollipop.techo.data.BaseTechoItem
 import com.lollipop.techo.data.SplitItem
+import com.lollipop.techo.databinding.PanelSplitSelectBinding
 import com.lollipop.techo.edit.EditDelegate
 
 /**
  * @author lollipop
  * @date 2021/12/23 22:36
  */
-class SplitEditDelegate: EditDelegate() {
+class SplitEditDelegate : EditDelegate() {
+
+    private var binding: PanelSplitSelectBinding? = null
 
     override fun isSupport(info: BaseTechoItem): Boolean {
         return info is SplitItem
     }
 
     override fun onCreateView(container: ViewGroup): View {
-        TODO("Not yet implemented")
+        val newBinding: PanelSplitSelectBinding = container.bind(false)
+        binding = newBinding
+        return newBinding.root
     }
 }
