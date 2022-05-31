35b41481cc681dda0c134f5a83d1fcb6144d7277
lollipop <liujian594@hotmail.com>
2022-05-31 20:39:01 +0800

    增加首页item的点击操作

--- app/src/main/java/com/lollipop/techo/activity/TechoDetailActivity.kt
+++ app/src/main/java/com/lollipop/techo/activity/TechoDetailActivity.kt
@@ -1,5 +1,6 @@
 package com.lollipop.techo.activity
 
+import android.content.Intent
 import android.view.View
 import com.lollipop.base.util.lazyBind
 import com.lollipop.techo.databinding.ActivityTechoDetailBinding
@@ -8,6 +9,25 @@ import com.lollipop.techo.databinding.ActivityTechoDetailBinding
  * 详情页
  */
 class TechoDetailActivity : HeaderActivity() {
+
+    companion object {
+
+        private const val PARAMETER_TECHO_ID = "PARAMETER_TECHO_ID"
+
+        private const val RESULT_TECHO_ID = "RESULT_TECHO_ID"
+
+        const val NO_ID = 0
+
+        fun putParams(intent: Intent, infoId: Int = 0): Intent {
+            return intent.putExtra(PARAMETER_TECHO_ID, infoId)
+        }
+
+        fun getResultTechoId(intent: Intent?): Int {
+            intent ?: return NO_ID
+            return intent.getIntExtra(RESULT_TECHO_ID, NO_ID)
+        }
+
+    }
 
     private val viewBinding: ActivityTechoDetailBinding by lazyBind()
 
--- app/src/main/java/com/lollipop/techo/list/home/TechoHolder.kt
+++ app/src/main/java/com/lollipop/techo/list/home/TechoHolder.kt
@@ -1,8 +1,12 @@
 package com.lollipop.techo.list.home
 
+import android.content.Context
+import android.content.Intent
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.lollipop.base.util.bind
+import com.lollipop.base.util.onClick
+import com.lollipop.techo.activity.TechoDetailActivity
 import com.lollipop.techo.data.BaseTextItem
 import com.lollipop.techo.data.TechoInfo
 import com.lollipop.techo.databinding.ItemHomeTechoBinding
@@ -25,12 +29,24 @@ class TechoHolder(
         Calendar.getInstance()
     }
 
+    private var techoId = 0
+
     init {
         binding.dayNumberView.setTypeface(FontHelper.Type.HomeListDay)
         binding.monthNumberView.setTypeface(FontHelper.Type.HomeListMonth)
+        itemView.onClick {
+            onItemViewClick(it.context)
+        }
+    }
+
+    private fun onItemViewClick(context: Context) {
+        context.startActivity(Intent(context, TechoDetailActivity::class.java).apply {
+            TechoDetailActivity.putParams(this, techoId)
+        })
     }
 
     fun bind(info: TechoInfo) {
+        techoId = info.id
         calendar.timeInMillis = info.updateTime
         binding.dayNumberView.text = calendar.get(Calendar.DAY_OF_MONTH).formatNumber()
         val monthNumber = calendar.get(Calendar.MONTH) + 1
