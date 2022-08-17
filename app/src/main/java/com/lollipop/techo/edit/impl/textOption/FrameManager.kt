6310a24ea94b03d34e9a7b3c2cd34b8dc3a2cacb
lollipop <liujian594@hotmail.com>
2022-08-17 20:32:26 +0800

    feat: 调整编辑页面的代码结构

--- app/src/main/java/com/lollipop/techo/edit/impl/textOption/FrameManager.kt
+++ app/src/main/java/com/lollipop/techo/edit/impl/textOption/FrameManager.kt
@@ -2,15 +2,20 @@ package com.lollipop.techo.edit.impl.textOption
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.lollipop.base.list.ItemTouchState
 import com.lollipop.base.list.OnItemMoveCallback
 import com.lollipop.base.list.OnItemSwipeCallback
 import com.lollipop.base.list.OnItemTouchStateChangedListener
+import com.lollipop.base.util.bind
 import com.lollipop.techo.R
 import com.lollipop.techo.data.FontStyle
 import com.lollipop.techo.data.TextSpan
+import com.lollipop.techo.databinding.ItemPanelTextOptionFrameAddBinding
+import com.lollipop.techo.databinding.ItemPanelTextOptionFrameBinding
 import java.util.*
 
 internal class FrameManager(
@@ -149,6 +154,74 @@ internal class FrameManager(
         if (status == ItemTouchState.IDLE) {
             updatePreview()
             updateOptionButton()
+        }
+    }
+
+    internal class FontStyleFrameAdapter(
+        private val list: List<TextSpan>,
+        private val textProvider: (TextSpan) -> CharSequence,
+        private val isSelected: (TextSpan) -> Boolean,
+        private val onAddButtonClick: () -> Unit,
+        private val onFrameClick: (Int) -> Unit
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        companion object {
+            private const val STYLE_ADD = 0
+            private const val STYLE_FRAME = 1
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return when (viewType) {
+                STYLE_ADD -> {
+                    AddFontStyleFrameHolder(parent.bind(), onAddButtonClick)
+                }
+                else -> {
+                    FontStyleFrameHolder(parent.bind(), onFrameClick)
+                }
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            if (holder is FontStyleFrameHolder) {
+                val textSpan = list[position]
+                holder.bind(textProvider(textSpan), isSelected(textSpan))
+            }
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            if (position >= list.size) {
+                return STYLE_ADD
+            }
+            return STYLE_FRAME
+        }
+
+        override fun getItemCount(): Int {
+            return list.size + 1
+        }
+
+    }
+
+    private class FontStyleFrameHolder(
+        private val binding: ItemPanelTextOptionFrameBinding,
+        private val onItemClickListener: (Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener { onItemClickListener(adapterPosition) }
+        }
+
+        fun bind(text: CharSequence, isSelected: Boolean) {
+            binding.labelView.text = text
+            binding.selectedIcon.isVisible = isSelected
+        }
+    }
+
+    private class AddFontStyleFrameHolder(
+        binding: ItemPanelTextOptionFrameAddBinding,
+        private val onItemClickListener: () -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            itemView.setOnClickListener { onItemClickListener() }
         }
     }
 
