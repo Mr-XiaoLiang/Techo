0f874222c7ba22aff51d523957320625ad6ae06b
lollipop <liujian594@hotmail.com>
2022-09-07 22:03:13 +0800

    feat: 增加了富文本操作面板的布局

--- app/src/main/java/com/lollipop/techo/fragment/RichTextOptionFragment.kt
+++ app/src/main/java/com/lollipop/techo/fragment/RichTextOptionFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.lollipop.base.util.WindowInsetsHelper
 import com.lollipop.base.util.fixInsetsByMargin
 import com.lollipop.base.util.lazyBind
+import com.lollipop.techo.R
 import com.lollipop.techo.databinding.FragmentRichTextOptionBinding
 
 class RichTextOptionFragment : PageFragment() {
@@ -25,6 +27,44 @@ class RichTextOptionFragment : PageFragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.backButton.fixInsetsByMargin(WindowInsetsHelper.Edge.HEADER)
         bindBackButton(binding.backButton)
+        initView()
+    }
+
+    private fun initView() {
+        binding.panelMenuBar.setOnItemSelectedListener {
+            onPanelSelected(it.itemId)
+        }
+    }
+
+    private fun onPanelSelected(itemId: Int): Boolean {
+        with(binding) {
+            selectorPanel.isVisible = false
+            layerPanel.isVisible = false
+            richOptionPanel.isVisible = false
+            palettePanel.isVisible = false
+            textSizePanel.isVisible = false
+        }
+        when (itemId) {
+            R.id.menuSelector -> {
+                binding.selectorPanel.isVisible = true
+            }
+            R.id.menuLayer -> {
+                binding.layerPanel.isVisible = true
+            }
+            R.id.menuFontSize -> {
+                binding.textSizePanel.isVisible = true
+            }
+            R.id.menuRichStyle -> {
+                binding.richOptionPanel.isVisible = true
+            }
+            R.id.menuPalette -> {
+                binding.palettePanel.isVisible = true
+            }
+            else -> {
+                return false
+            }
+        }
+        return true
     }
 
 }
