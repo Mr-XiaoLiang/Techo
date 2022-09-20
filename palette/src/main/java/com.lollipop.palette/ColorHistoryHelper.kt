08e1318b483691460da3e8ed43e48e075955c8e0
lollipop <liujian594@hotmail.com>
2022-09-20 21:52:01 +0800

    feat: 开始制作本地的颜色记录工具

--- palette/src/main/java/com.lollipop.palette/ColorHistoryHelper.kt
+++ palette/src/main/java/com.lollipop.palette/ColorHistoryHelper.kt
@@ -1,5 +1,8 @@
 package com.lollipop.palette
 
+import org.json.JSONArray
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -10,13 +13,86 @@ class ColorHistoryHelper {
         val colorHistory = ArrayList<Int>()
 
         fun write(outputStream: OutputStream) {
-            ColorHistoryWriter.write(colorHistory, outputStream)
+            var inputStream: InputStream? = null
+            try {
+                val jsonArray = JSONArray()
+                colorHistory.forEach {
+                    jsonArray.put(it)
+                }
+                val input = ByteArrayInputStream(jsonArray.toString().toByteArray())
+                inputStream = input
+                val output = outputStream
+
+                val buffer = ByteArray(1024 * 4)
+                do {
+                    val length = input.read(buffer)
+                    if (length < 0) {
+                        break
+                    }
+                    output.write(buffer, 0, length)
+                } while (true)
+                output.flush()
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            } finally {
+                try {
+                    inputStream?.close()
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+                try {
+                    outputStream.close()
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
         }
 
         fun read(inputStream: InputStream) {
-            val read = ColorHistoryWriter.read(inputStream)
+            val tempList = ArrayList<Int>()
+            var outputStream: OutputStream? = null
+            try {
+                val output = ByteArrayOutputStream()
+                outputStream = output
+
+                val input = inputStream
+                val buffer = ByteArray(1024 * 4)
+                do {
+                    val length = input.read(buffer)
+                    if (length < 0) {
+                        break
+                    }
+                    output.write(buffer, 0, length)
+                } while (true)
+                output.flush()
+
+                val jsonValue = outputStream.toString()
+                if (jsonValue.isNotEmpty()) {
+                    val jsonArray = JSONArray(jsonValue)
+                    for (index in 0 until jsonArray.length()) {
+                        val c = jsonArray.optInt(index)
+                        if (c != 0) {
+                            tempList.add(c)
+                        }
+                    }
+                }
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            } finally {
+                try {
+                    inputStream.close()
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+                try {
+                    outputStream?.close()
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
+
             colorHistory.clear()
-            colorHistory.addAll(read)
+            colorHistory.addAll(tempList)
         }
 
     }
