790d7b42681ec21787c2441f16bd32d9e8185281
lollipop <liujian594@hotmail.com>
2022-10-06 20:57:38 +0800

    feat: 改一下demo，还没调试

--- app/src/main/java/com/lollipop/techo/activity/DemoActivity.kt
+++ app/src/main/java/com/lollipop/techo/activity/DemoActivity.kt
@@ -1,27 +1,21 @@
 package com.lollipop.techo.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lollipop.base.util.WindowInsetsHelper
 import com.lollipop.base.util.lazyBind
 import com.lollipop.base.util.registerResult
 import com.lollipop.techo.databinding.ActivityDemoBinding
-import com.lollipop.techo.fragment.RichTextOptionFragment
 
 class DemoActivity : AppCompatActivity() {
 
     private val binding: ActivityDemoBinding by lazyBind()
 
-    private val richTextOptionLauncher = registerResult(
-        RichTextOptionFragment.LAUNCHER
-    ) {
-
-    }
-
     private val recorderLauncher = registerResult(
         RecorderActivity.LAUNCHER
     ) {
-
+        Toast.makeText(this, it?.path ?: "null", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,20 +26,7 @@ class DemoActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        val stringBuilder = StringBuilder()
-        for (i in 0..20) {
-            stringBuilder.append("测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容测试内容")
-        }
-        val string = stringBuilder.toString()
         binding.openButton.setOnClickListener {
-//            richTextOptionLauncher.launch(
-//                RichTextOptionFragment.Request(
-//                    1,
-//                    TechoItem.Text().apply {
-//                        value = string
-//                    }
-//                )
-//            )
             recorderLauncher.launch(false)
         }
     }
