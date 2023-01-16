fae96bb102105684d84e25b354c694f6c141a137
lollipop <liujian594@hotmail.com>
2023-01-17 00:38:48 +0800

    feat: 调整浏览器页面对于系统窗口的兼容，BottomSheetBehavior是真的烦

--- browser/src/main/java/com/lollipop/browser/MainActivity.kt
+++ browser/src/main/java/com/lollipop/browser/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.lollipop.browser
 
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.View
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.lollipop.base.util.dp2px
+import com.lollipop.base.util.insets.*
 import com.lollipop.base.util.lazyBind
-import com.lollipop.base.util.onClick
+import com.lollipop.base.util.lazyLogD
 import com.lollipop.browser.databinding.ActivityMainBinding
 import com.lollipop.web.WebHelper
 import com.lollipop.web.WebHost
+import kotlin.math.max
 
 class MainActivity : AppCompatActivity(), WebHost {
 
@@ -27,16 +29,51 @@ class MainActivity : AppCompatActivity(), WebHost {
 //            }
 //    }
 
+    private val log by lazyLogD()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowInsetsHelper.initWindowFlag(this)
         setContentView(binding.root)
         initBottomSheetPanel()
+        binding.statusPanel.fixInsetsByListener(
+            WindowInsetsEdgeViewDelegate(
+                direction = WindowInsetsEdgeViewDelegate.Direction.TOP,
+                minSize = 4.dp2px
+            )
+        )
+        binding.statusPanel.setBackgroundColor(getColor(R.color.teal_700))
+
     }
 
     private fun initBottomSheetPanel() {
         val sheetBehavior = BottomSheetBehavior.from(binding.navBottomSheetPanel)
         sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
         sheetBehavior.peekHeight = resources.getDimensionPixelSize(R.dimen.bottom_action_bar_height)
+        sheetBehavior.isHideable = false
+
+        val panelOperator = WindowInsetsOperator(WindowInsetsEdge.CONTENT)
+        panelOperator.snapshotPadding(binding.navBottomSheetPanel)
+
+        binding.root.fixInsetsByListener { v, _, insets ->
+            val res = v.resources
+            val actionHeight = res.getDimensionPixelSize(R.dimen.bottom_action_bar_height)
+
+            // res.getDimensionPixelSize(R.dimen.mtrl_min_touch_target_size)
+            val minHeight = 48.dp2px
+
+            val value = WindowInsetsHelper.getInsetsValue(
+                insets,
+                WindowInsetsType.MANDATORY_SYSTEM_GESTURES
+            )
+            val peekHeight = max(value.bottom + minHeight - actionHeight, 0)
+            WindowInsetsHelper.setHeight(binding.navigationBarBackgroundView, peekHeight)
+            panelOperator.padding(
+                binding.navBottomSheetPanel,
+                bottom = panelOperator.basePadding.bottom + peekHeight
+            )
+            insets
+        }
     }
 
     private fun createWebPage(url: String): View {
@@ -44,4 +81,5 @@ class MainActivity : AppCompatActivity(), WebHost {
         WebHelper.bind(this, webView).init().loadUrl(url)
         return webView
     }
+
 }
