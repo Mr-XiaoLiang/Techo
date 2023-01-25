a64f5cb72a73ec532ca5b03a1d6552c892040aa9
lollipop <liujian594@hotmail.com>
2023-01-26 01:55:10 +0800

    feat: 完善FragmentSwitcher的代码实现

--- fragmentHelper/src/main/java/com/lollipop/fragment/FragmentHelper.kt
+++ fragmentHelper/src/main/java/com/lollipop/fragment/FragmentHelper.kt
@@ -1,7 +1,22 @@
 package com.lollipop.fragment
 
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
 object FragmentHelper {
 
+    fun with(fragmentManager: FragmentManager, container: ViewGroup): FragmentSwitcher {
+        return FragmentSwitcher(fragmentManager, container.id)
+    }
 
+    fun with(activity: AppCompatActivity, container: ViewGroup): FragmentSwitcher {
+        return with(activity.supportFragmentManager, container)
+    }
+
+    fun with(fragment: Fragment, container: ViewGroup): FragmentSwitcher {
+        return with(fragment.childFragmentManager, container)
+    }
 
 }
