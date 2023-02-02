947ed283c9af3f07130947c2ff90198d4f3054db
lollipop <liujian594@hotmail.com>
2023-02-02 20:43:47 +0800

    feat: 增加WebStatusManager的本地数据存储功能

--- browser/src/main/java/com/lollipop/browser/web/WebStatusManager.kt
+++ browser/src/main/java/com/lollipop/browser/web/WebStatusManager.kt
@@ -1,6 +1,11 @@
 package com.lollipop.browser.web
 
 import android.content.Context
+import com.lollipop.base.util.ListenerManager
+import com.lollipop.base.util.TxtHelper
+import com.lollipop.base.util.doAsync
+import com.lollipop.base.util.onUI
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.File
 import java.util.*
@@ -10,17 +15,107 @@ import java.util.*
  */
 object WebStatusManager {
 
-    private val statusFile: File? = null
+    private var statusFile: File? = null
+
+    private const val STATUS_FILE_NAME = "status.ltf"
 
     private val statusArray = ArrayList<WebStatus>()
     private val statusMap = HashMap<String, WebStatus>()
 
+    private val readyListener = ListenerManager<OnManagerReadyListener>()
+
+    var isReady = false
+        private set
+
     fun resume(context: Context) {
-        // TODO 读取并且初始化状态管理区
+        init(context)
+        loadData()
+    }
+
+    private fun init(context: Context) {
+        statusFile = File(context.filesDir, STATUS_FILE_NAME)
+    }
+
+    private fun loadData() {
+        doAsync {
+            if (loadDataFromFile()) {
+                onUI {
+                    onReady()
+                }
+            }
+        }
+    }
+
+    private fun loadDataFromFile(): Boolean {
+        try {
+            val file = statusFile ?: return false
+            if (!file.exists()) {
+                clearStatus()
+                return true
+            }
+            val value = TxtHelper.readFromFile(file)
+            val jsonArray = JSONArray(value)
+            val list = ArrayList<WebStatus>()
+            for (i in 0 until jsonArray.length()) {
+                val obj = jsonArray.optJSONObject(i) ?: continue
+                try {
+                    val status = WebStatus.fromJson(obj)
+                    if (status.pageId.isNotEmpty()) {
+                        list.add(status)
+                    }
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
+            resetStatus(list)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            clearStatus()
+        }
+        return true
+    }
+
+    private fun clearStatus() {
+        synchronized(WebStatusManager) {
+            statusArray.clear()
+            statusMap.clear()
+        }
+    }
+
+    private fun resetStatus(list: List<WebStatus>) {
+        synchronized(WebStatusManager) {
+            statusArray.clear()
+            statusMap.clear()
+            list.forEach {
+                statusArray.add(it)
+                statusMap[it.pageId] = it
+            }
+        }
+    }
+
+    private fun onReady() {
+        synchronized(WebStatusManager) {
+            isReady = true
+            readyListener.invoke { it.onStatusManagerReady() }
+            readyListener.clear()
+        }
     }
 
     fun save() {
-        // TODO 保存状态到本地
+        val file = statusFile ?: return
+        doAsync {
+            val tempList = ArrayList(statusArray)
+            val jsonArray = JSONArray()
+            tempList.forEach {
+                if (it.pageId.isNotEmpty()) {
+                    jsonArray.put(it.toJson())
+                }
+            }
+            if (file.exists()) {
+                file.delete()
+            }
+            TxtHelper.writeToFile(jsonArray.toString(), file)
+        }
     }
 
     fun newPage(): String {
@@ -39,20 +134,38 @@ object WebStatusManager {
 
     private fun addPage(pageId: String) {
         val webStatus = WebStatus(pageId = pageId)
-        statusArray.add(webStatus)
-        statusMap[pageId] = webStatus
+        synchronized(WebStatusManager) {
+            statusArray.add(webStatus)
+            statusMap[pageId] = webStatus
+        }
     }
 
     fun removePage(pageId: String) {
-        val status = statusMap[pageId] ?: return
-        statusArray.remove(status)
-        removePageCache(status)
+        synchronized(WebStatusManager) {
+            val status = statusMap[pageId] ?: return
+            statusArray.remove(status)
+            removePageCache(status)
+        }
     }
 
     private fun removePageCache(status: WebStatus) {
         status.favicon?.tryDelete()
         status.snapshot?.tryDelete()
         // TODO
+    }
+
+    fun addReadyListener(listener: OnManagerReadyListener) {
+        synchronized(WebStatusManager) {
+            if (isReady) {
+                listener.onStatusManagerReady()
+                if (!readyListener.isEmpty) {
+                    readyListener.invoke { it.onStatusManagerReady() }
+                    readyListener.clear()
+                }
+                return
+            }
+            readyListener.addListener(listener)
+        }
     }
 
     private fun File.tryDelete() {
@@ -134,6 +247,10 @@ object WebStatusManager {
             }
         }
 
+    }
+
+    fun interface OnManagerReadyListener {
+        fun onStatusManagerReady()
     }
 
 }
