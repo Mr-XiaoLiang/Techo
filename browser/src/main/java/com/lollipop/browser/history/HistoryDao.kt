709791701425422b027aefc5687bec4bb85bc814
lollipop <liujian594@hotmail.com>
2023-02-06 21:37:12 +0800

    feat: 为历史记录引入Room数据库

--- browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
@@ -1,8 +1,16 @@
 package com.lollipop.browser.history
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface HistoryDao {
+
+    @Query("SELECT * FROM history Limit :pageSize Offset :pageOffset")
+    fun select(pageOffset: Int, pageSize: Int): List<HistoryInfo>
+
+    @Insert
+    fun insert(info: HistoryInfo)
 
 }
--- browser/src/main/java/com/lollipop/browser/history/HistoryInfo.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryInfo.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "history")
 data class HistoryInfo(
     @PrimaryKey val uid: Int = 0,
     @ColumnInfo(name = "url") val url: String = "",
