13ebaf150d6e3ee84627d503fd67592471f58a02
lollipop <liujian594@hotmail.com>
2023-02-07 21:18:33 +0800

    feat: 不用Room了，不习惯

--- browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
@@ -1,16 +1,31 @@
 package com.lollipop.browser.history
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import com.lollipop.base.util.DatabaseHelper
 
-@Dao
-interface HistoryDao {
+class HistoryDao {
 
-    @Query("SELECT * FROM history Limit :pageSize Offset :pageOffset")
-    fun select(pageOffset: Int, pageSize: Int): List<HistoryInfo>
+//    @Query("SELECT * FROM history Limit :pageSize Offset :pageOffset")
+//    fun select(pageOffset: Int, pageSize: Int): List<HistoryInfo>
+//
+//    @Insert
+//    fun insert(info: HistoryInfo)
 
-    @Insert
-    fun insert(info: HistoryInfo)
+    private object HistoryTable {
+//        id: Int,
+//        url: String,
+//        title: String,
+//        time: Long,
+//        favicon: String
+
+        const val NAME = "History"
+        const val ID = "ID"
+
+        enum class Column(val format: DatabaseHelper.ColumnFormat) {
+            TITLE(DatabaseHelper.ColumnFormat.TEXT),
+            URL(DatabaseHelper.ColumnFormat.INTEGER),
+            TIME(DatabaseHelper.ColumnFormat.INTEGER),
+            FAVICON(DatabaseHelper.ColumnFormat.TEXT),
+        }
+    }
 
 }
--- browser/src/main/java/com/lollipop/browser/history/HistoryInfo.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryInfo.kt
@@ -1,14 +1,9 @@
 package com.lollipop.browser.history
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "history")
 data class HistoryInfo(
-    @PrimaryKey val uid: Int = 0,
-    @ColumnInfo(name = "url") val url: String = "",
-    @ColumnInfo(name = "title") val title: String = "",
-    @ColumnInfo(name = "time") val time: Long = 0L,
-    @ColumnInfo(name = "favicon") val favicon: String = ""
+    val id: Int,
+    val url: String,
+    val title: String,
+    val time: Long,
+    val favicon: String
 )
