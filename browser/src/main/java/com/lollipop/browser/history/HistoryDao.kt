512a3b878f6561e6fcda23ec595ef8ccc5a2ce71
lollipop <liujian594@hotmail.com>
2023-02-08 23:03:24 +0800

    feat: 使用新的数据库工具来写历史管理

--- browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
@@ -1,31 +1,55 @@
 package com.lollipop.browser.history
 
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import com.lollipop.base.util.DatabaseHelper
 
-class HistoryDao {
+class HistoryDao(context: Context) : DatabaseHelper(context, DB_NAME, null, VERSION) {
 
-//    @Query("SELECT * FROM history Limit :pageSize Offset :pageOffset")
-//    fun select(pageOffset: Int, pageSize: Int): List<HistoryInfo>
-//
-//    @Insert
-//    fun insert(info: HistoryInfo)
+    companion object {
+        private const val DB_NAME = "history"
+        private const val VERSION = 1
+    }
 
-    private object HistoryTable {
-//        id: Int,
-//        url: String,
-//        title: String,
-//        time: Long,
-//        favicon: String
+    override fun getTableArray(): Array<Table> {
+        return arrayOf(HistoryTable)
+    }
 
-        const val NAME = "History"
-        const val ID = "ID"
+    private object HistoryTable: Table() {
 
-        enum class Column(val format: DatabaseHelper.ColumnFormat) {
-            TITLE(DatabaseHelper.ColumnFormat.TEXT),
-            URL(DatabaseHelper.ColumnFormat.INTEGER),
-            TIME(DatabaseHelper.ColumnFormat.INTEGER),
-            FAVICON(DatabaseHelper.ColumnFormat.TEXT),
+        override val tableName: String = "History"
+        override val columns: Array<out ColumnEnum>
+            get() {
+                return Column.values()
+            }
+
+        fun getCreateSql(): String {
+            return createSql(tableName, idColumn, Column.values())
         }
+
+        private fun queryHistoryList(
+            db: SQLiteDatabase,
+            rawQueryCallback: (SQLiteDatabase) -> Cursor,
+        ): List<HistoryInfo> {
+            return queryList(db, rawQueryCallback) {
+                HistoryInfo(
+                    id = it.getIntByName(idColumn),
+                    url = it.getTextByName(Column.URL),
+                    title = it.getTextByName(Column.TITLE),
+                    favicon = it.getTextByName(Column.TITLE),
+                    time = it.getLongByName(Column.TITLE),
+                )
+            }
+        }
+
+        enum class Column(override val format: ColumnFormat): ColumnEnum {
+            TITLE(ColumnFormat.TEXT),
+            URL(ColumnFormat.INTEGER),
+            TIME(ColumnFormat.INTEGER),
+            FAVICON(ColumnFormat.TEXT),
+        }
+
     }
 
 }
