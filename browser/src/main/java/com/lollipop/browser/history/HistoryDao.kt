92ea3f23dcde4cad456eb5592968db35c774cd94
lollipop <liujian594@hotmail.com>
2023-02-09 21:57:11 +0800

    feat: 增加历史记录的数据库操作方法

--- browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
+++ browser/src/main/java/com/lollipop/browser/history/HistoryDao.kt
@@ -1,5 +1,6 @@
 package com.lollipop.browser.history
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
@@ -16,7 +17,23 @@ class HistoryDao(context: Context) : DatabaseHelper(context, DB_NAME, null, VERS
         return arrayOf(HistoryTable)
     }
 
-    private object HistoryTable: Table() {
+    fun select(pageIndex: Int, pageSize: Int = 20): List<HistoryInfo> {
+        return HistoryTable.selectByPage(readDb, pageIndex, pageSize)
+    }
+
+    fun insert(info: HistoryInfo, needId: Boolean = false): Int {
+        return HistoryTable.insert(writeDb, info, needId)
+    }
+
+    fun delete(id: Int): Int {
+        return HistoryTable.delete(writeDb, id)
+    }
+
+    fun deleteAll(db: SQLiteDatabase): Int {
+        return HistoryTable.deleteAll(db)
+    }
+
+    private object HistoryTable : Table() {
 
         override val tableName: String = "History"
         override val columns: Array<out ColumnEnum>
@@ -24,8 +41,60 @@ class HistoryDao(context: Context) : DatabaseHelper(context, DB_NAME, null, VERS
                 return Column.values()
             }
 
-        fun getCreateSql(): String {
-            return createSql(tableName, idColumn, Column.values())
+        private val SELECT_ALL: String by lazy {
+            val sqlBuilder = StringBuilder().append("SELECT $idColumn ")
+            columns.forEach {
+                sqlBuilder.append(", ")
+                sqlBuilder.append(it.name)
+            }
+            sqlBuilder.append(" FROM ")
+                .append(tableName)
+                .append(" ORDER BY ")
+                .append(idColumn)
+                .append(" DESC LIMIT ? OFFSET ? ")
+            sqlBuilder.toString()
+        }
+
+        fun selectByPage(db: SQLiteDatabase, pageIndex: Int, pageSize: Int): List<HistoryInfo> {
+            return queryHistoryList(db) {
+                it.rawQuery(
+                    SELECT_ALL,
+                    arrayOf(pageSize.toString(), (pageIndex * pageSize).toString())
+                )
+            }
+        }
+
+        fun insert(db: SQLiteDatabase, info: HistoryInfo, needId: Boolean = false): Int {
+            val insert = db.insert(
+                tableName,
+                null,
+                ContentValues().apply {
+                    putValue(Column.URL, info.url)
+                    putValue(Column.FAVICON, info.favicon)
+                    putValue(Column.TIME, info.time)
+                    putValue(Column.TITLE, info.title)
+                }
+            )
+            if (needId && insert >= 0) {
+                return selectLastId(tableName, idColumn, db)
+            }
+            return 0
+        }
+
+        fun delete(db: SQLiteDatabase, id: Int): Int {
+            return db.delete(
+                tableName,
+                "$idColumn = ?",
+                arrayOf(id.toString())
+            )
+        }
+
+        fun deleteAll(db: SQLiteDatabase): Int {
+            return db.delete(
+                tableName,
+                null,
+                null
+            )
         }
 
         private fun queryHistoryList(
@@ -43,7 +112,7 @@ class HistoryDao(context: Context) : DatabaseHelper(context, DB_NAME, null, VERS
             }
         }
 
-        enum class Column(override val format: ColumnFormat): ColumnEnum {
+        enum class Column(override val format: ColumnFormat) : ColumnEnum {
             TITLE(ColumnFormat.TEXT),
             URL(ColumnFormat.INTEGER),
             TIME(ColumnFormat.INTEGER),
