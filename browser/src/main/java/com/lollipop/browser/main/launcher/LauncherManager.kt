017f52214a5684e87165f783d2398a6723b7cdd1
lollipop <liujian594@hotmail.com>
2023-02-14 21:57:14 +0800

    feat: 首页需要缺省的launch内容

--- browser/src/main/java/com/lollipop/browser/main/launcher/LauncherManager.kt
+++ browser/src/main/java/com/lollipop/browser/main/launcher/LauncherManager.kt
@@ -1,5 +1,10 @@
 package com.lollipop.browser.main.launcher
 
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.lollipop.browser.R
+
 object LauncherManager {
 
     fun load(callback: (List<LauncherInfo>) -> Unit) {
@@ -12,6 +17,46 @@ object LauncherManager {
 
     fun move(info: LauncherInfo, index: Int) {
         TODO()
+    }
+
+    private enum class DefaultLauncher(
+        val id: Int,
+        @StringRes
+        val label: Int,
+        @DrawableRes
+        val icon: Int,
+        @ColorRes
+        val iconTint: Int,
+        @DrawableRes
+        val background: Int,
+        val url: String
+    ) {
+
+        Baidu(
+            id = -1,
+            label = R.string.label_baidu,
+            icon = R.drawable.ic_baidu,
+            iconTint = R.color.gray_0,
+            background = 0,
+            url = ""
+        ),
+        Bing(
+            id = -2,
+            label = R.string.label_bing,
+            icon = 0,
+            iconTint = R.color.gray_0,
+            background = 0,
+            url = ""
+        ),
+        Google(
+            id = -3,
+            label = R.string.label_google,
+            icon = 0,
+            iconTint = R.color.gray_0,
+            background = 0,
+            url = ""
+        ),
+
     }
 
 }
