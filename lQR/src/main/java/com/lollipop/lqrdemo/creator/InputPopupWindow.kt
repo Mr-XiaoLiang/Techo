a2db195fd46403e4219c5ffb8abba28b22854143
Lollipop <liujian594@hotmail.com>
2023-04-28 22:47:00 +0800

    feat: 增加输入框的Dialog

---
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/InputPopupWindow.kt
@@ -0,0 +1,42 @@
+package com.lollipop.lqrdemo.creator
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.lollipop.base.util.insets.WindowInsetsEdge
+import com.lollipop.base.util.insets.WindowInsetsHelper
+import com.lollipop.base.util.insets.WindowInsetsType
+import com.lollipop.base.util.insets.fixInsetsByPadding
+import com.lollipop.base.util.lazyBind
+import com.lollipop.lqrdemo.databinding.DialogInputBinding
+
+class InputPopupWindow(context: Context) : Dialog(context) {
+
+    private val binding: DialogInputBinding by lazyBind()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window?.let {
+            WindowInsetsHelper.initWindowFlag(it)
+            updateWindowAttributes(it)
+        }
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.inputContent.fixInsetsByPadding(WindowInsetsEdge.CONTENT).apply {
+            windowInsetsOperator.insetsType = WindowInsetsType.IME
+        }
+    }
+
+    private fun updateWindowAttributes(window: Window) {
+        window.setType(WindowManager.LayoutParams.TYPE_APPLICATION_PANEL)
+        window.attributes = window.attributes.apply {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+            }
+        }
+    }
+
+}
