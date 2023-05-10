24c6aaf5803373b302a9e43ed986e896aa0ee193
Lollipop <liujian594@hotmail.com>
2023-05-10 22:02:31 +0800

    feat: 决定让主题框架适配系统壁纸颜色的接口，改造框架。但是没想好怎么生成其他颜色

--- pigment/src/main/java/com/lollipop/pigment/Pigment.kt
+++ pigment/src/main/java/com/lollipop/pigment/Pigment.kt
@@ -1,5 +1,7 @@
 package com.lollipop.pigment
 
+import android.graphics.Color
+
 /**
  * 颜料
  * 他表示装饰主题色变化的内容
@@ -8,46 +10,66 @@ class Pigment(
     /**
      * 主题色
      */
-    val primary: Int,
-
-    /**
-     * 主题色变体
-     * 一般表示比主题色更深或者更浅的颜色，
-     * 用于表达主题色，但是和主题色区分
-     */
-    val primaryVariant: Int,
-
-    /**
-     * 在主题色之上的内容的颜色
-     */
-    val onPrimaryTitle: Int,
-
-    /**
-     * 在主题色之上的内容的颜色
-     */
-    val onPrimaryBody: Int,
+    val primary: Color,
 
     /**
      * 次要颜色
      * 它是一种撞色
      * 用于装饰主题色空间内容醒目元素
      */
-    val secondary: Int,
+    val secondary: Color?,
 
+    /**
+     * 次要颜色
+     * 它是一种撞色
+     * 用于装饰主题色空间内容醒目元素
+     */
+    val tertiary: Color?
+
+) {
+
+    val primaryColor: Int by lazy {
+        primary.toArgb()
+    }
+
+    val secondaryColor: Int by lazy {
+        secondary?.toArgb() ?: primaryColor
+    }
+
+    val tertiaryColor: Int by lazy {
+        tertiary?.toArgb() ?: secondaryColor
+    }
+
+    /**
+     * 主题色变体
+     * 一般表示比主题色更深或者更浅的颜色，
+     * 用于表达主题色，但是和主题色区分
+     */
+//    val primaryVariant: Int,
+
+    /**
+     * 在主题色之上的内容的颜色
+     */
+//    val onPrimaryTitle: Int,
+
+    /**
+     * 在主题色之上的内容的颜色
+     */
+//    val onPrimaryBody: Int,
     /**
      * 次要颜色的变体
      * 一般是次要颜色的加深或是变浅
      * 用于表达次要颜色的同时和次要颜色区分
      */
-    val secondaryVariant: Int,
+//    val secondaryVariant: Int,
 
     /**
      * 在次要颜色之上的内容的颜色
      */
-    val onSecondaryTitle: Int,
+//    val onSecondaryTitle: Int,
 
     /**
      * 在次要颜色之上的内容的颜色
      */
-    val onSecondaryBody: Int,
-)
+//    val onSecondaryBody: Int,
+}
--- pigment/src/main/java/com/lollipop/pigment/PigmentWallpaperCenter.kt
+++ pigment/src/main/java/com/lollipop/pigment/PigmentWallpaperCenter.kt
@@ -3,9 +3,11 @@ package com.lollipop.pigment
 import android.app.WallpaperColors
 import android.app.WallpaperManager
 import android.content.Context
-import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 object PigmentWallpaperCenter {
 
@@ -30,33 +32,31 @@ object PigmentWallpaperCenter {
     }
 
     fun init(context: Context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            val application = context.applicationContext
-            val wallpaper = application.getSystemService(Context.WALLPAPER_SERVICE)
-            if (wallpaper is WallpaperManager) {
-                val wallpaperColors = wallpaper.getWallpaperColors(WallpaperManager.FLAG_SYSTEM)
-                if (wallpaperColors != null) {
-                    parseColorToPigment(wallpaperColors)
-                }
-                wallpaper.addOnColorsChangedListener(
-                    WallpaperManager.OnColorsChangedListener { colors, which ->
-                        if (colors != null && which == WallpaperManager.FLAG_SYSTEM) {
-                            parseColorToPigment(colors)
-                        }
-                    },
-                    Handler(Looper.getMainLooper())
-                )
+        val application = context.applicationContext
+        val wallpaper = application.getSystemService(Context.WALLPAPER_SERVICE)
+        if (wallpaper is WallpaperManager) {
+            val wallpaperColors = wallpaper.getWallpaperColors(WallpaperManager.FLAG_SYSTEM)
+            if (wallpaperColors != null) {
+                parseColorToPigment(wallpaperColors)
             }
+            wallpaper.addOnColorsChangedListener(
+                WallpaperManager.OnColorsChangedListener { colors, which ->
+                    if (colors != null && which == WallpaperManager.FLAG_SYSTEM) {
+                        parseColorToPigment(colors)
+                    }
+                },
+                Handler(Looper.getMainLooper())
+            )
         }
     }
 
     private fun parseColorToPigment(wallpaperColors: WallpaperColors) {
-//        val newPigment = Pigment(
-//            primary = wallpaperColors.primaryColor.toArgb(),
-//
-//        )
-//        onPigmentChanged(newPigment)
-        // 没想好怎么弄，颜色值不够
+        val newPigment = Pigment(
+            primary = wallpaperColors.primaryColor,
+            secondary = wallpaperColors.secondaryColor,
+            tertiary = wallpaperColors.tertiaryColor
+        )
+        onWallpaperPigmentChanged(newPigment)
     }
 
     private fun onWallpaperPigmentChanged(newPigment: Pigment) {
@@ -78,6 +78,42 @@ object PigmentWallpaperCenter {
 
     fun unregisterPigment(page: PigmentPage) {
         pigmentProviderHelper.unregisterPigment(page)
+    }
+
+    fun registerPigmentByLifecycle(lifecycle: Lifecycle, page: PigmentPage): LifecyclePigmentPage {
+        return LifecyclePigmentPage(lifecycle, page)
+    }
+
+    class LifecyclePigmentPage internal constructor(
+        lifecycle: Lifecycle,
+        private val page: PigmentPage
+    ) : LifecycleEventObserver, PigmentPage {
+
+        init {
+            val currentState = lifecycle.currentState
+            if (currentState.isAtLeast(Lifecycle.State.RESUMED)) {
+                registerPigment(this)
+            }
+            lifecycle.addObserver(this)
+        }
+
+        override fun onDecorationChanged(pigment: Pigment) {
+            page.onDecorationChanged(pigment)
+        }
+
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> {
+                    registerPigment(this)
+                }
+
+                Lifecycle.Event.ON_PAUSE -> {
+                    unregisterPigment(this)
+                }
+
+                else -> {}
+            }
+        }
     }
 
 }
