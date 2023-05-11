6dee10fb6bcf8bef5aee8a2c9dd68e8b0e4c0b11
Lollipop <liujian594@hotmail.com>
2023-05-11 23:37:53 +0800

    feat: 也许可以通过HSV颜色的明度和饱和度来转换颜色

---
+++ pigment/src/main/java/com/lollipop/pigment/PigmentColorHelper.kt
@@ -0,0 +1,17 @@
+package com.lollipop.pigment
+
+object PigmentColorHelper {
+
+    fun variant(src: Int): Int {
+        TODO()
+    }
+
+    fun title(src: Int): Int {
+        TODO()
+    }
+
+    fun body(src: Int): Int {
+        TODO()
+    }
+
+}
