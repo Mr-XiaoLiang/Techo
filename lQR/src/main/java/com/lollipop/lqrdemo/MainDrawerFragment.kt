db38ab0e60a1e6588506d998f9fe4f2b609092da
Lollipop <liujian594@hotmail.com>
2023-05-20 18:55:56 +0800

    feat: 增加首页的侧面抽屉，这是个目录

---
+++ lQR/src/main/java/com/lollipop/lqrdemo/MainDrawerFragment.kt
@@ -0,0 +1,94 @@
+package com.lollipop.lqrdemo
+
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lollipop.base.util.bind
+import com.lollipop.base.util.lazyBind
+import com.lollipop.lqrdemo.base.BaseFragment
+import com.lollipop.lqrdemo.databinding.FragmentMainDrawerBinding
+import com.lollipop.lqrdemo.databinding.ItemMainDrawerBinding
+import com.lollipop.pigment.Pigment
+import com.lollipop.pigment.PigmentWallpaperCenter
+
+class MainDrawerFragment : BaseFragment() {
+
+    private val binding: FragmentMainDrawerBinding by lazyBind()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recyclerView.layoutManager = LinearLayoutManager(
+            view.context, RecyclerView.VERTICAL, false
+        )
+        binding.recyclerView.adapter = ItemAdapter(getItemList())
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onDecorationChanged(pigment: Pigment) {
+        super.onDecorationChanged(pigment)
+        binding.root.setBackgroundColor(pigment.backgroundColor)
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+        binding.logoImageView.setBackgroundColor(pigment.secondaryColor)
+        binding.logoImageView.imageTintList = ColorStateList.valueOf(pigment.onSecondaryTitle)
+    }
+
+    private fun getItemList(): List<Item> {
+        return listOf(
+            // TODO
+        )
+    }
+
+    private class ItemAdapter(
+        private val itemList: List<Item>,
+    ) : RecyclerView.Adapter<ItemHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+            return ItemHolder(parent.bind(false))
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+        override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+            holder.bind(itemList[position])
+        }
+
+    }
+
+
+    private class ItemHolder(
+        private val binding: ItemMainDrawerBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Item) {
+            PigmentWallpaperCenter.pigment?.let {
+                binding.itemTextView.setTextColor(it.onBackgroundTitle)
+            }
+            binding.itemTextView.setText(item.value)
+            binding.itemTextView.setOnClickListener {
+                item.onClickCallback()
+            }
+        }
+    }
+
+
+    private class Item(
+        @StringRes
+        val value: Int,
+        val onClickCallback: () -> Unit,
+    )
+
+}
--- lQR/src/main/java/com/lollipop/lqrdemo/base/BaseFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/base/BaseFragment.kt
@@ -1,10 +1,29 @@
 package com.lollipop.lqrdemo.base
 
 import androidx.fragment.app.Fragment
+import com.lollipop.base.util.checkCallback
 import com.lollipop.pigment.Pigment
 import com.lollipop.pigment.PigmentPage
+import com.lollipop.pigment.PigmentProvider
 
-open class BaseFragment: Fragment(), PigmentPage {
+open class BaseFragment : Fragment(), PigmentPage {
+
+    private var superPigmentProvider: PigmentProvider? = null
+
+    override fun onResume() {
+        super.onResume()
+        checkCallback<PigmentProvider>(context) {
+            superPigmentProvider = it
+            it.registerPigment(this)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        superPigmentProvider?.unregisterPigment(this)
+        superPigmentProvider = null
+    }
+
     override fun onDecorationChanged(pigment: Pigment) {
     }
 }
