583493118de95712d60bc1b98c77a3a20bfae59f
Lollipop <liujian594@hotmail.com>
2023-05-23 20:42:58 +0800

    feat: 增加关于的页面，随便做个页面留个名吧

--- lQR/src/main/java/com/lollipop/lqrdemo/other/AboutActivity.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/other/AboutActivity.kt
@@ -1,9 +1,12 @@
 package com.lollipop.lqrdemo.other
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import com.lollipop.base.util.lazyBind
+import com.lollipop.base.util.versionName
 import com.lollipop.lqrdemo.base.ColorModeActivity
 import com.lollipop.lqrdemo.databinding.ActivityAboutBinding
+import com.lollipop.pigment.Pigment
 
 class AboutActivity : ColorModeActivity() {
 
@@ -12,5 +15,18 @@ class AboutActivity : ColorModeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.versionView.text = versionName()
+    }
+
+    override fun onDecorationChanged(pigment: Pigment) {
+        super.onDecorationChanged(pigment)
+        binding.root.setBackgroundColor(pigment.backgroundColor)
+        binding.backButton.imageTintList = ColorStateList.valueOf(pigment.onBackgroundTitle)
+        binding.titleView.setTextColor(pigment.onBackgroundTitle)
+        binding.logoImageView.setBackgroundColor(pigment.secondaryColor)
+        binding.logoImageView.imageTintList = ColorStateList.valueOf(pigment.onSecondaryTitle)
+        binding.andrewView.setTextColor(pigment.onBackgroundBody)
+        binding.lollipopView.setTextColor(pigment.onBackgroundBody)
+        binding.versionView.setTextColor(pigment.onBackgroundBody)
     }
 }
