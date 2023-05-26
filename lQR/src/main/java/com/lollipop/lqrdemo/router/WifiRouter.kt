a7e03c6dd45c3b0071957bf1d55adea3c39a1a8d
Lollipop <liujian594@hotmail.com>
2023-05-27 00:00:34 +0800

    feat: 不能自动加入Wi-Fi就手动复制密码好了

--- lQR/src/main/java/com/lollipop/lqrdemo/router/WifiRouter.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/router/WifiRouter.kt
@@ -1,41 +1,24 @@
 package com.lollipop.lqrdemo.router
 
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import android.widget.Toast
+import com.lollipop.base.util.Clipboard
+import com.lollipop.lqrdemo.R
 import com.lollipop.qr.comm.BarcodeInfo
+
 
 object WifiRouter : BarcodeRouter<BarcodeInfo.Wifi>() {
 
-    // 需要权限，不搞了
-//    override fun open(context: Context, barcodeInfo: BarcodeInfo.Wifi): Boolean {
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-//            return false
-//        }
-//        val suggestionsList =
-//            if (barcodeInfo.encryptionType == BarcodeInfo.Wifi.EncryptionType.OPEN) {
-//                listOf(
-//                    WifiNetworkSuggestion.Builder()
-//                        .setSsid(barcodeInfo.ssid)
-//                        .setIsAppInteractionRequired(false) // Optional (Needs location permission)
-//                        .build()
-//                )
-//            } else {
-//                listOf(
-//                    WifiNetworkSuggestion.Builder()
-//                        .setSsid(barcodeInfo.ssid)
-//                        .setWpa2Passphrase(barcodeInfo.password)
-//                        .setIsAppInteractionRequired(false) // Optional (Needs location permission)
-//                        .build(),
-//                    WifiNetworkSuggestion.Builder()
-//                        .setSsid(barcodeInfo.ssid)
-//                        .setWpa3Passphrase(barcodeInfo.password)
-//                        .setIsAppInteractionRequired(false) // Optional (Needs location permission)
-//                        .build()
-//                )
-//            }
-//
-//        val wifiManager =
-//            context.getSystemService(Context.WIFI_SERVICE) as? WifiManager ?: return false
-//        wifiManager.addNetworkSuggestions(suggestionsList)
-//        return super.open(context, barcodeInfo)
-//    }
+    override fun getIntent(context: Context, barcodeInfo: BarcodeInfo.Wifi): Intent {
+        Clipboard.copy(context, value = barcodeInfo.password)
+        Toast.makeText(
+            context,
+            context.getString(R.string.copied_wifi, barcodeInfo.ssid),
+            Toast.LENGTH_LONG
+        ).show()
+        return Intent(Settings.ACTION_WIFI_SETTINGS)
+    }
 
 }
