3805843ee3a0f631f2cf77d51e421adcd46d078d
Lollipop <liujian594@hotmail.com>
2023-06-05 23:40:16 +0800

    feat: 增加状态栏的快速入口

---
+++ lQR/src/main/java/com/lollipop/lqrdemo/other/ScanTileService.kt
@@ -0,0 +1,12 @@
+package com.lollipop.lqrdemo.other
+
+import android.service.quicksettings.TileService
+
+class ScanTileService: TileService() {
+
+    override fun onClick() {
+        super.onClick()
+        // TODO
+    }
+
+}
