77ba356a16413ff901bcf18d423f59eb1f6bb706
lollipop <liujian594@hotmail.com>
2023-06-21 22:13:38 +0800

    feat: 增加二维码图片保存的方法，但是还没试好不好用

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/QrCreatorHelper.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/QrCreatorHelper.kt
@@ -1,11 +1,21 @@
 package com.lollipop.lqrdemo.creator
 
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.lifecycle.LifecycleOwner
 import com.lollipop.base.util.ListenerManager
 import com.lollipop.lqrdemo.creator.bridge.OnCodeContentChangedListener
 import com.lollipop.lqrdemo.creator.bridge.OnCodeMatrixChangedListener
 import com.lollipop.qr.writer.BarcodeWriter
 import com.lollipop.qr.writer.LBitMatrix
+import java.io.OutputStream
+import kotlin.random.Random
 
 class QrCreatorHelper(private val lifecycleOwner: LifecycleOwner) {
 
@@ -19,6 +29,54 @@ class QrCreatorHelper(private val lifecycleOwner: LifecycleOwner) {
 
     private val codeMatrixChangedListener = ListenerManager<OnCodeMatrixChangedListener>()
     private val codeContentChangedListener = ListenerManager<OnCodeContentChangedListener>()
+
+    companion object {
+
+        private fun getFileName(): String {
+            val fileCode = Random.nextInt(0xF0000, 0xFFFFF).toString(16).uppercase()
+            return "LQR_$fileCode.png"
+        }
+
+        fun saveToMediaStore(context: Context, bitmap: Bitmap) {
+            val contentResolver = context.applicationContext.contentResolver
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                saveImageInQ(bitmap, contentResolver)
+            } else {
+                MediaStore.Images.Media.insertImage(
+                    contentResolver, bitmap,
+                    getFileName(),
+                    ""
+                )
+            }
+        }
+
+        private fun saveImageInQ(bitmap: Bitmap, contentResolver: ContentResolver): Uri? {
+            val filename = getFileName()
+            val fos: OutputStream?
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+                put(MediaStore.MediaColumns.IS_PENDING, 1)
+            }
+
+            //use application context to get contentResolver
+            val uri =
+                contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            uri?.let { contentResolver.openOutputStream(it) }.also { fos = it }
+            fos?.use { bitmap.compress(Bitmap.CompressFormat.PNG, 100, it) }
+            fos?.flush()
+            fos?.close()
+
+            contentValues.clear()
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+            uri?.let {
+                contentResolver.update(it, contentValues, null, null)
+            }
+            return uri
+        }
+    }
+
 
     private fun onContentChanged() {
         val content = contentValue
