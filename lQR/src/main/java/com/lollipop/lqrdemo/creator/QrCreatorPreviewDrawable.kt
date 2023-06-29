3c3c3de9a62607bf4032f56f58416727da971dc0
lollipop <liujian594@hotmail.com>
2023-06-29 21:28:02 +0800

    feat: 边做边想好痛苦呀

---
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/QrCreatorPreviewDrawable.kt
@@ -0,0 +1,24 @@
+package com.lollipop.lqrdemo.creator
+
+import android.graphics.Canvas
+import android.graphics.ColorFilter
+import android.graphics.PixelFormat
+import android.graphics.drawable.Drawable
+
+class QrCreatorPreviewDrawable: Drawable() {
+    override fun draw(canvas: Canvas) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setAlpha(alpha: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getOpacity(): Int {
+        return PixelFormat.TRANSPARENT
+    }
+}
