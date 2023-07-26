dcf18395ebc48b2998b90ca1463d1b4f3e47d774
lollipop <liujian594@hotmail.com>
2023-07-26 21:23:00 +0800

    feat: 创建一个布局，先占位。然后需要做一个调色板的Dialog出来

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/QrBackgroundFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/QrBackgroundFragment.kt
@@ -1,9 +1,26 @@
 package com.lollipop.lqrdemo.creator
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lollipop.base.util.lazyBind
 import com.lollipop.lqrdemo.base.BaseFragment
+import com.lollipop.lqrdemo.databinding.FragmentQrEditBackgroundBinding
 
 /**
  * 背景的设置
  */
 class QrBackgroundFragment : BaseFragment() {
+
+    private val binding: FragmentQrEditBackgroundBinding by lazyBind()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
 }
