b845743cac5ff9fe65d1525648b3ce080dc1bd70
lollipop <liujian594@hotmail.com>
2023-07-28 21:45:00 +0800

    feat: 关于页面增加版权链接

--- lQR/src/main/java/com/lollipop/lqrdemo/other/AboutActivity.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/other/AboutActivity.kt
@@ -1,15 +1,22 @@
 package com.lollipop.lqrdemo.other
 
+import android.content.Intent
 import android.content.res.ColorStateList
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import com.lollipop.base.util.insets.WindowInsetsEdge
 import com.lollipop.base.util.insets.WindowInsetsHelper
 import com.lollipop.base.util.insets.fixInsetsByPadding
 import com.lollipop.base.util.lazyBind
+import com.lollipop.base.util.richtext.RichText
 import com.lollipop.base.util.versionName
 import com.lollipop.lqrdemo.base.ColorModeActivity
 import com.lollipop.lqrdemo.databinding.ActivityAboutBinding
+import com.lollipop.pigment.BlendMode
 import com.lollipop.pigment.Pigment
+import com.lollipop.pigment.PigmentWallpaperCenter
+
 
 class AboutActivity : ColorModeActivity() {
 
@@ -22,6 +29,16 @@ class AboutActivity : ColorModeActivity() {
         binding.root.fixInsetsByPadding(WindowInsetsEdge.ALL)
         binding.versionView.text = versionName()
         bindByBack(binding.backButton)
+
+        val richFlow = RichText.startRichFlow()
+        val linkColor = Color.BLUE // PigmentWallpaperCenter.pigment?.secondaryColor ?: Color.BLUE
+        Icons.values().forEachIndexed { index, icons ->
+            if (index > 0) {
+                richFlow.addInfo(", ")
+            }
+            richFlow.addClickInfo(icons.value, linkColor, icons.url, ::onLinkClick)
+        }
+        richFlow.into(binding.copyrightView)
     }
 
     override fun onDecorationChanged(pigment: Pigment) {
@@ -35,4 +52,20 @@ class AboutActivity : ColorModeActivity() {
         binding.lollipopView.setTextColor(pigment.onBackgroundBody)
         binding.versionView.setTextColor(pigment.onBackgroundBody)
     }
+
+    private fun onLinkClick(link: String) {
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(link)))
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+
+    private enum class Icons(val value: String, val url: String) {
+
+        Material("material", "https://m3.material.io/styles/icons/overview"),
+//        Icons8("icons8", "https://icons8.com")
+
+    }
+
 }
