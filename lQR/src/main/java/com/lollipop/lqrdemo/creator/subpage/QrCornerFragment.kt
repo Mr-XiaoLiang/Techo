b8c62dfe7a7e99659c772b7495a39889126a6044
lollipop <liujian594@hotmail.com>
2023-08-30 21:27:37 +0800

    完成了圆角设置的布局，但是内容的设置还需要变一下

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/QrCornerFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/QrCornerFragment.kt
@@ -1,9 +1,25 @@
 package com.lollipop.lqrdemo.creator.subpage
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lollipop.base.util.lazyBind
+import com.lollipop.lqrdemo.databinding.FragmentQrCornerBinding
+
 /**
  * 圆角设置的页面，它和背景设置其实是一起的，但是业务上，放不到同一个页面里面
  */
 class QrCornerFragment : QrBaseSubpageFragment() {
 
+    private val binding: FragmentQrCornerBinding by lazyBind()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
 
 }
