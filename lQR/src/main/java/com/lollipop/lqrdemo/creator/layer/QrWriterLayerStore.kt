bf0ff4d9cf9fe9ba659e7380470ec3561102b798
lollipop <liujian594@hotmail.com>
2023-10-17 23:14:51 +0800

    feat: 增加中央Layer管理器的一些实现，在赶公司项目，少写点

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/layer/QrWriterLayerStore.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/layer/QrWriterLayerStore.kt
@@ -4,10 +4,30 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import com.lollipop.base.util.ListenerManager
+import com.lollipop.lqrdemo.creator.writer.QrWriterLayer
 
 object QrWriterLayerStore {
 
     private val listenerManager = ListenerManager<Fork>()
+
+    private var alignment: Class<QrWriterLayer>? = null
+    private var content: Class<QrWriterLayer>? = null
+    private var position: Class<QrWriterLayer>? = null
+
+    fun setAlignmentLayer(clazz: Class<QrWriterLayer>?) {
+        alignment = clazz
+        listenerManager.invoke { it.onAlignmentLayerChanged(clazz) }
+    }
+
+    fun setContentLayer(clazz: Class<QrWriterLayer>?) {
+        content = clazz
+        listenerManager.invoke { it.onContentLayerChanged(clazz) }
+    }
+
+    fun setPositionLayer(clazz: Class<QrWriterLayer>?) {
+        position = clazz
+        listenerManager.invoke { it.onPositionLayerChanged(clazz) }
+    }
 
     fun fork(
         lifecycle: Lifecycle,
@@ -19,12 +39,27 @@ object QrWriterLayerStore {
         }
         listenerManager.addListener(fork)
         lifecycle.addObserver(fork)
+        fork.onAlignmentLayerChanged(alignment)
+        fork.onContentLayerChanged(content)
+        fork.onPositionLayerChanged(position)
         return fork
     }
 
     class Fork(
         private val minLifecycle: Lifecycle.State
     ) : LifecycleEventObserver {
+
+        fun onAlignmentLayerChanged(clazz: Class<QrWriterLayer>?) {
+            // TODO
+        }
+
+        fun onContentLayerChanged(clazz: Class<QrWriterLayer>?) {
+            // TODO
+        }
+
+        fun onPositionLayerChanged(clazz: Class<QrWriterLayer>?) {
+            // TODO
+        }
 
         override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
             if (event.targetState <= minLifecycle) {
