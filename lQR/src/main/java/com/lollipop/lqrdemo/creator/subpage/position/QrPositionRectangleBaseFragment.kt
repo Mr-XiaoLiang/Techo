d42b1e36b1b950d268190bddd232234a15240308
lollipop <liujian594@hotmail.com>
2023-12-15 20:57:16 +0800

    feat: 简单改了下实现，具体的绘制还没写

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/position/QrPositionRectangleBaseFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/position/QrPositionRectangleBaseFragment.kt
@@ -1,5 +1,8 @@
 package com.lollipop.lqrdemo.creator.subpage.position
 
+import android.graphics.Color
+import androidx.annotation.ColorInt
+import androidx.annotation.FloatRange
 import com.lollipop.lqrdemo.creator.layer.BitMatrixWriterLayer
 import com.lollipop.lqrdemo.creator.layer.PositionWriterLayer
 import com.lollipop.lqrdemo.creator.subpage.adjust.StyleAdjustContentFragment
@@ -17,19 +20,37 @@ open class QrPositionRectangleBaseFragment : StyleAdjustContentFragment() {
         const val BORDER_RADIUS_MIN = 0F
         const val CORE_RADIUS_MIN = 0F
 
+        var BORDER_COLOR = Color.BLACK
+        var CORE_COLOR = Color.BLACK
+
     }
 
     // notifyContentChanged()
 
     abstract class BaseLayer : BitMatrixWriterLayer(), PositionWriterLayer {
 
+        @get:FloatRange(from = BORDER_RADIUS_MIN.toDouble(), to = BORDER_RADIUS_MAX.toDouble())
         open val borderRadius: Float
             get() {
                 return BORDER_RADIUS
             }
+
+        @get:FloatRange(from = CORE_RADIUS_MIN.toDouble(), to = CORE_RADIUS_MAX.toDouble())
         open val coreRadius: Float
             get() {
                 return CORE_RADIUS
+            }
+
+        @get:ColorInt
+        open val borderColor: Int
+            get() {
+                return BORDER_COLOR
+            }
+
+        @get:ColorInt
+        open val coreColor: Int
+            get() {
+                return CORE_COLOR
             }
 
     }
