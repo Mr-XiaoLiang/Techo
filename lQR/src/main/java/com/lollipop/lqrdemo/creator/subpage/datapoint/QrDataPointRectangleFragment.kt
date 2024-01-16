9294e84aada206c2e420428aa4ff37371f7b861d
lollipop <liujian594@hotmail.com>
2024-01-16 19:39:18 +0800

    feat: 完成圆形数据点的绘制

--- lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/datapoint/QrDataPointRectangleFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/datapoint/QrDataPointRectangleFragment.kt
@@ -1,40 +1,31 @@
 package com.lollipop.lqrdemo.creator.subpage.datapoint
 
-import android.annotation.SuppressLint
-import android.content.res.ColorStateList
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.Rect
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.annotation.ColorInt
-import androidx.annotation.FloatRange
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.slider.Slider
-import com.lollipop.base.util.dp2px
 import com.lollipop.base.util.lazyBind
 import com.lollipop.base.util.onClick
-import com.lollipop.lqrdemo.R
 import com.lollipop.lqrdemo.base.PigmentTheme
 import com.lollipop.lqrdemo.creator.HistoryColor
 import com.lollipop.lqrdemo.creator.PaletteDialog
-import com.lollipop.lqrdemo.creator.layer.AlignmentWriterLayer
 import com.lollipop.lqrdemo.creator.layer.BitMatrixWriterLayer
+import com.lollipop.lqrdemo.creator.layer.ContentWriterLayer
 import com.lollipop.lqrdemo.creator.subpage.adjust.StyleAdjustContentFragment
 import com.lollipop.lqrdemo.databinding.FragmentQrDataPointRectangleBinding
-import com.lollipop.lqrdemo.view.CheckedButtonBackgroundDrawable
 import com.lollipop.pigment.Pigment
 import com.lollipop.qr.writer.LBitMatrix
 import com.lollipop.qr.writer.LQrBitMatrix
-import com.lollipop.widget.RoundBackgroundView
+import kotlin.math.min
 
 class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
 
@@ -45,17 +36,14 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
         @ColorInt
         private var COLOR: Int = Color.BLACK
 
-        const val RADIUS_MAX = 1F
-        const val RADIUS_MIN = 0F
-
     }
 
     private val binding: FragmentQrDataPointRectangleBinding by lazyBind()
 
-    private val checkedMap = HashMap<Int, Boolean>()
-    private val historyColorAdapter = HistoryColorAdapter(::onColorChanged, ::onPaletteClick) {
-        currentPigment
-    }
+    private val historyColorAdapter =
+        createHistoryColorAdapter(::onColorChanged, ::onPaletteClick) {
+            currentPigment
+        }
 
     private val sliderBackPressure = Runnable {
         invokeSliderChanged()
@@ -138,73 +126,20 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
     private fun invokeSliderChanged() {
         // slider里面是0~100的，我们需要的是0~1，所以需要缩小100倍
         val value = binding.radiusSlider.value * 0.01F
-        changeRadius(value)
+        changeRadius(
+            info = RADIUS,
+            value = value,
+            leftTop = binding.leftTopCoreCornerButton.isChecked,
+            rightTop = binding.rightTopCoreCornerButton.isChecked,
+            rightBottom = binding.rightBottomCoreCornerButton.isChecked,
+            leftBottom = binding.leftBottomCoreCornerButton.isChecked
+        )
         notifyContentChanged()
     }
 
     private fun changeColor(value: Int) {
         COLOR = value
     }
-
-    private fun changeRadius(
-        value: Float,
-    ) {
-        val info = RADIUS
-        val leftTop = binding.leftTopCoreCornerButton.isChecked
-        val rightTop = binding.rightTopCoreCornerButton.isChecked
-        val rightBottom = binding.rightBottomCoreCornerButton.isChecked
-        val leftBottom = binding.leftBottomCoreCornerButton.isChecked
-        if (leftTop) {
-            info.leftTopX = value
-            info.leftTopY = value
-        }
-        if (rightTop) {
-            info.rightTopX = value
-            info.rightTopY = value
-        }
-        if (rightBottom) {
-            info.rightBottomX = value
-            info.rightBottomY = value
-        }
-        if (leftBottom) {
-            info.leftBottomX = value
-            info.leftBottomY = value
-        }
-    }
-
-    private fun View.checkedToggle() {
-        // 读取状态并反转
-        val view = this
-        view.isChecked = !view.isChecked
-    }
-
-    private fun View.updateCheckedState() {
-        // 走一遍方法，让状态更新
-        val view = this
-        view.isChecked = view.isChecked
-    }
-
-    private var View.isChecked: Boolean
-        get() {
-            val view = this
-            if (view.id == View.NO_ID) {
-                return false
-            }
-            return checkedMap[view.id] ?: false
-        }
-        set(value) {
-            val view = this
-            if (view.id == View.NO_ID) {
-                view.alpha = 0.5F
-                return
-            }
-            checkedMap[view.id] = value
-            view.alpha = if (value) {
-                1F
-            } else {
-                0.5F
-            }
-        }
 
     override fun onDecorationChanged(pigment: Pigment) {
         super.onDecorationChanged(pigment)
@@ -234,19 +169,7 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
             }
     }
 
-    private fun checkOrUpdateButton(btnView: View, iconView: ImageView, color: Int) {
-        iconView.imageTintList = ColorStateList.valueOf(color)
-        val background = btnView.background
-        if (background is CheckedButtonBackgroundDrawable) {
-            background.color = color
-        } else {
-            val drawable = CheckedButtonBackgroundDrawable()
-            drawable.color = color
-            btnView.background = drawable
-        }
-    }
-
-    class Layer : BitMatrixWriterLayer(), AlignmentWriterLayer {
+    class Layer : BitMatrixWriterLayer(), ContentWriterLayer {
 
         private val radius = Radius()
         private var color = Color.BLACK
@@ -255,7 +178,7 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
 
         private val contentPaint = Paint()
 
-        override fun drawAlignment(canvas: Canvas) {
+        override fun drawContent(canvas: Canvas) {
             checkContentPath()
             contentPaint.setColor(color)
             contentPaint.style = Paint.Style.FILL
@@ -273,6 +196,7 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
         }
 
         private fun updateDataPointPath() {
+            radius.copyFrom(RADIUS)
             val path = contentPath
             path.reset()
             findQrBitMatrix { matrix ->
@@ -289,19 +213,9 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
                         }
                         val currentType = matrix.getType(x, y)
                         if (currentType == LBitMatrix.Type.BLACK) {
-                            val edge =
-                                matrix.getVerticalEdge(x, y, LBitMatrix.Type.BLACK) { px, py ->
-                                    !isInAlignmentPattern(matrix, px, py)
-                                }
-                            if (edge < 0) {
-                                y++
-                                continue
-                            }
-                            if (edge >= y) {
-                                tempRect.set(x, y, x, edge)
-                                addRectToPathByScale(path, tempRect)
-                            }
-                            y = edge + 1
+                            tempRect.set(x, y, x, y)
+                            addPoint(path, tempRect)
+                            y++
                         } else {
                             val edge =
                                 matrix.getVerticalEdge(x, y, LBitMatrix.Type.WHITE) { px, py ->
@@ -316,6 +230,25 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
                     }
                 }
             }
+        }
+
+        private fun addPoint(path: Path, rect: Rect) {
+            val leftEdgeByScale = getLeftEdgeByScale(rect.left.toFloat())
+            val topEdgeByScale = getTopEdgeByScale(rect.top.toFloat())
+            val rightEdgeByScale = getRightEdgeByScale(rect.right.toFloat())
+            val bottomEdgeByScale = getBottomEdgeByScale(rect.bottom.toFloat())
+            val d = min(
+                rightEdgeByScale - leftEdgeByScale,
+                bottomEdgeByScale - topEdgeByScale
+            )
+            path.addRoundRect(
+                leftEdgeByScale,
+                topEdgeByScale,
+                rightEdgeByScale,
+                bottomEdgeByScale,
+                radius.pixelSize(d, d),
+                Path.Direction.CW
+            )
         }
 
         private fun isInAlignmentPattern(matrix: LQrBitMatrix, x: Int, y: Int): Boolean {
@@ -335,226 +268,6 @@ class QrDataPointRectangleFragment : StyleAdjustContentFragment() {
             if (!radius.isSame(RADIUS)) {
                 updateDataPointPath()
             }
-        }
-
-    }
-
-    private class HistoryColorAdapter(
-        private val onColorClick: (Int) -> Unit,
-        private val onPaletteClick: () -> Unit,
-        private val getPigment: () -> Pigment?,
-    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-        companion object {
-            private const val TYPE_COLOR = 0
-            private const val TYPE_PALETTE = 1
-        }
-
-        private val colorList = ArrayList<Int>()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            if (viewType == TYPE_PALETTE) {
-                return PaletteHolder.create(parent, onPaletteClick)
-            }
-            return ColorHolder.crate(parent, ::onItemClick)
-        }
-
-        private fun onItemClick(position: Int) {
-            if (position < 0 || position >= colorList.size) {
-                return
-            }
-            val color = colorList[position]
-            onColorClick(color)
-        }
-
-        @SuppressLint("NotifyDataSetChanged")
-        fun onColorListChanged(list: List<Int>) {
-            colorList.clear()
-            colorList.addAll(list)
-            notifyDataSetChanged()
-        }
-
-        fun findFirstColor(): Int {
-            if (colorList.isNotEmpty()) {
-                return colorList[0]
-            }
-            return Color.RED
-        }
-
-        override fun getItemCount(): Int {
-            return colorList.size + 1
-        }
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            if (holder is ColorHolder) {
-                holder.bind(colorList[position])
-            }
-            if (holder is PaletteHolder) {
-                holder.bind(getPigment())
-            }
-        }
-
-        override fun getItemViewType(position: Int): Int {
-            if (position >= 0 && position < colorList.size) {
-                return TYPE_COLOR
-            }
-            return TYPE_PALETTE
-        }
-
-    }
-
-    private class ColorHolder(
-        view: View,
-        private val colorView: RoundBackgroundView,
-        private val onClickCallback: (Int) -> Unit
-    ) : RecyclerView.ViewHolder(view) {
-
-        companion object {
-            fun crate(parent: ViewGroup, onClickCallback: (Int) -> Unit): ColorHolder {
-                val frameLayout = FrameLayout(parent.context)
-                frameLayout.layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                val colorView = RoundBackgroundView(parent.context)
-                colorView.type = RoundBackgroundView.RoundType.SMALLER
-                colorView.value = 0.5F
-                val margin = 6.dp2px
-                colorView.layoutParams = FrameLayout.LayoutParams(
-                    36.dp2px,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                ).apply {
-                    marginStart = margin
-                    marginEnd = margin
-                }
-                frameLayout.addView(colorView)
-                return ColorHolder(frameLayout, colorView, onClickCallback)
-            }
-        }
-
-        init {
-            colorView.onClick {
-                onItemViewClick()
-            }
-        }
-
-        private fun onItemViewClick() {
-            onClickCallback(adapterPosition)
-        }
-
-        fun bind(color: Int) {
-            colorView.color = color
-        }
-
-    }
-
-    private class PaletteHolder(
-        view: View,
-        private val iconView: ImageView,
-        private val onClickCallback: () -> Unit
-    ) : RecyclerView.ViewHolder(view) {
-
-        companion object {
-            fun create(parent: ViewGroup, onClickCallback: () -> Unit): PaletteHolder {
-                val frameLayout = FrameLayout(parent.context)
-                frameLayout.layoutParams = ViewGroup.LayoutParams(
-                    48.dp2px,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                val iconView = ImageView(parent.context)
-                val size = 24.dp2px
-                iconView.layoutParams = FrameLayout.LayoutParams(size, size).apply {
-                    gravity = Gravity.CENTER
-                }
-                iconView.setImageResource(R.drawable.ic_baseline_palette_24)
-                frameLayout.addView(iconView)
-                return PaletteHolder(frameLayout, iconView, onClickCallback)
-            }
-        }
-
-        init {
-            itemView.onClick {
-                onClickCallback()
-            }
-        }
-
-        fun bind(pigment: Pigment?) {
-            if (pigment == null) {
-                iconView.imageTintList = null
-            } else {
-                iconView.imageTintList
-                ColorStateList.valueOf(pigment.onBackgroundTitle)
-            }
-        }
-
-    }
-
-    private class Radius(
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftTopX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftTopY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightTopX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightTopY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightBottomX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightBottomY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftBottomX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftBottomY: Float = 0F,
-    ) {
-
-        fun array(): FloatArray {
-            return floatArrayOf(
-                leftTopX,
-                leftTopY,
-                rightTopX,
-                rightTopY,
-                rightBottomX,
-                rightBottomY,
-                leftBottomX,
-                leftBottomY,
-            )
-        }
-
-        fun pixelSize(width: Float, height: Float): FloatArray {
-            return floatArrayOf(
-                leftTopX * width,
-                leftTopY * height,
-                rightTopX * width,
-                rightTopY * height,
-                rightBottomX * width,
-                rightBottomY * height,
-                leftBottomX * width,
-                leftBottomY * height,
-            )
-        }
-
-        fun copyFrom(info: Radius) {
-            this.leftTopX = info.leftTopX
-            this.leftTopY = info.leftTopY
-            this.rightTopX = info.rightTopX
-            this.rightTopY = info.rightTopY
-            this.rightBottomX = info.rightBottomX
-            this.rightBottomY = info.rightBottomY
-            this.leftBottomX = info.leftBottomX
-            this.leftBottomY = info.leftBottomY
-        }
-
-        fun isSame(info: Radius): Boolean {
-            return (this.leftTopX == info.leftTopX &&
-                    this.leftTopY == info.leftTopY &&
-                    this.rightTopX == info.rightTopX &&
-                    this.rightTopY == info.rightTopY &&
-                    this.rightBottomX == info.rightBottomX &&
-                    this.rightBottomY == info.rightBottomY &&
-                    this.leftBottomX == info.leftBottomX &&
-                    this.leftBottomY == info.leftBottomY
-                    )
         }
 
     }
--- lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/position/QrPositionRectangleFragment.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/creator/subpage/position/QrPositionRectangleFragment.kt
@@ -59,15 +59,12 @@ class QrPositionRectangleFragment : StyleAdjustContentFragment() {
             coreColor = Color.BLACK
         )
 
-        const val RADIUS_MAX = 1F
-        const val RADIUS_MIN = 0F
-
     }
 
     private val binding: FragmentQrPositionRectangleBinding by lazyBind()
 
     private val checkedMap = HashMap<Int, Boolean>()
-    private val historyColorAdapter = HistoryColorAdapter(::onColorChanged, ::onPaletteClick) {
+    private val historyColorAdapter = createHistoryColorAdapter(::onColorChanged, ::onPaletteClick) {
         currentPigment
     }
 
@@ -227,66 +224,6 @@ class QrPositionRectangleFragment : StyleAdjustContentFragment() {
         )
     }
 
-    private fun changeRadius(
-        info: Radius,
-        value: Float,
-        leftTop: Boolean,
-        rightTop: Boolean,
-        rightBottom: Boolean,
-        leftBottom: Boolean,
-    ) {
-        if (leftTop) {
-            info.leftTopX = value
-            info.leftTopY = value
-        }
-        if (rightTop) {
-            info.rightTopX = value
-            info.rightTopY = value
-        }
-        if (rightBottom) {
-            info.rightBottomX = value
-            info.rightBottomY = value
-        }
-        if (leftBottom) {
-            info.leftBottomX = value
-            info.leftBottomY = value
-        }
-    }
-
-    private fun View.checkedToggle() {
-        // 读取状态并反转
-        val view = this
-        view.isChecked = !view.isChecked
-    }
-
-    private fun View.updateCheckedState() {
-        // 走一遍方法，让状态更新
-        val view = this
-        view.isChecked = view.isChecked
-    }
-
-    private var View.isChecked: Boolean
-        get() {
-            val view = this
-            if (view.id == View.NO_ID) {
-                return false
-            }
-            return checkedMap[view.id] ?: false
-        }
-        set(value) {
-            val view = this
-            if (view.id == View.NO_ID) {
-                view.alpha = 0.5F
-                return
-            }
-            checkedMap[view.id] = value
-            view.alpha = if (value) {
-                1F
-            } else {
-                0.5F
-            }
-        }
-
     override fun onDecorationChanged(pigment: Pigment) {
         super.onDecorationChanged(pigment)
         pigment.blendMode.startFlow(pigment.primaryColor)
@@ -350,18 +287,6 @@ class QrPositionRectangleFragment : StyleAdjustContentFragment() {
                 )
                 PigmentTheme.updateSlider(binding.radiusSlider, color)
             }
-    }
-
-    private fun checkOrUpdateButton(btnView: View, iconView: ImageView, color: Int) {
-        iconView.imageTintList = ColorStateList.valueOf(color)
-        val background = btnView.background
-        if (background is CheckedButtonBackgroundDrawable) {
-            background.color = color
-        } else {
-            val drawable = CheckedButtonBackgroundDrawable()
-            drawable.color = color
-            btnView.background = drawable
-        }
     }
 
     class Layer : BitMatrixWriterLayer(), PositionWriterLayer {
@@ -565,226 +490,6 @@ class QrPositionRectangleFragment : StyleAdjustContentFragment() {
                     this.coreRadius.isSame(info.coreRadius) &&
                     this.borderColor == info.borderColor &&
                     this.coreColor == info.coreColor
-                    )
-        }
-
-    }
-
-    private class HistoryColorAdapter(
-        private val onColorClick: (Int) -> Unit,
-        private val onPaletteClick: () -> Unit,
-        private val getPigment: () -> Pigment?,
-    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-        companion object {
-            private const val TYPE_COLOR = 0
-            private const val TYPE_PALETTE = 1
-        }
-
-        private val colorList = ArrayList<Int>()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            if (viewType == TYPE_PALETTE) {
-                return PaletteHolder.create(parent, onPaletteClick)
-            }
-            return ColorHolder.crate(parent, ::onItemClick)
-        }
-
-        private fun onItemClick(position: Int) {
-            if (position < 0 || position >= colorList.size) {
-                return
-            }
-            val color = colorList[position]
-            onColorClick(color)
-        }
-
-        @SuppressLint("NotifyDataSetChanged")
-        fun onColorListChanged(list: List<Int>) {
-            colorList.clear()
-            colorList.addAll(list)
-            notifyDataSetChanged()
-        }
-
-        fun findFirstColor(): Int {
-            if (colorList.isNotEmpty()) {
-                return colorList[0]
-            }
-            return Color.RED
-        }
-
-        override fun getItemCount(): Int {
-            return colorList.size + 1
-        }
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            if (holder is ColorHolder) {
-                holder.bind(colorList[position])
-            }
-            if (holder is PaletteHolder) {
-                holder.bind(getPigment())
-            }
-        }
-
-        override fun getItemViewType(position: Int): Int {
-            if (position >= 0 && position < colorList.size) {
-                return TYPE_COLOR
-            }
-            return TYPE_PALETTE
-        }
-
-    }
-
-    private class ColorHolder(
-        view: View,
-        private val colorView: RoundBackgroundView,
-        private val onClickCallback: (Int) -> Unit
-    ) : RecyclerView.ViewHolder(view) {
-
-        companion object {
-            fun crate(parent: ViewGroup, onClickCallback: (Int) -> Unit): ColorHolder {
-                val frameLayout = FrameLayout(parent.context)
-                frameLayout.layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                val colorView = RoundBackgroundView(parent.context)
-                colorView.type = RoundBackgroundView.RoundType.SMALLER
-                colorView.value = 0.5F
-                val margin = 6.dp2px
-                colorView.layoutParams = FrameLayout.LayoutParams(
-                    36.dp2px,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                ).apply {
-                    marginStart = margin
-                    marginEnd = margin
-                }
-                frameLayout.addView(colorView)
-                return ColorHolder(frameLayout, colorView, onClickCallback)
-            }
-        }
-
-        init {
-            colorView.onClick {
-                onItemViewClick()
-            }
-        }
-
-        private fun onItemViewClick() {
-            onClickCallback(adapterPosition)
-        }
-
-        fun bind(color: Int) {
-            colorView.color = color
-        }
-
-    }
-
-    private class PaletteHolder(
-        view: View,
-        private val iconView: ImageView,
-        private val onClickCallback: () -> Unit
-    ) : RecyclerView.ViewHolder(view) {
-
-        companion object {
-            fun create(parent: ViewGroup, onClickCallback: () -> Unit): PaletteHolder {
-                val frameLayout = FrameLayout(parent.context)
-                frameLayout.layoutParams = ViewGroup.LayoutParams(
-                    48.dp2px,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                val iconView = ImageView(parent.context)
-                val size = 24.dp2px
-                iconView.layoutParams = FrameLayout.LayoutParams(size, size).apply {
-                    gravity = Gravity.CENTER
-                }
-                iconView.setImageResource(R.drawable.ic_baseline_palette_24)
-                frameLayout.addView(iconView)
-                return PaletteHolder(frameLayout, iconView, onClickCallback)
-            }
-        }
-
-        init {
-            itemView.onClick {
-                onClickCallback()
-            }
-        }
-
-        fun bind(pigment: Pigment?) {
-            if (pigment == null) {
-                iconView.imageTintList = null
-            } else {
-                iconView.imageTintList
-                ColorStateList.valueOf(pigment.onBackgroundTitle)
-            }
-        }
-
-    }
-
-    private class Radius(
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftTopX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftTopY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightTopX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightTopY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightBottomX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var rightBottomY: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftBottomX: Float = 0F,
-        @FloatRange(from = RADIUS_MIN.toDouble(), to = RADIUS_MAX.toDouble())
-        var leftBottomY: Float = 0F,
-    ) {
-
-        fun array(): FloatArray {
-            return floatArrayOf(
-                leftTopX,
-                leftTopY,
-                rightTopX,
-                rightTopY,
-                rightBottomX,
-                rightBottomY,
-                leftBottomX,
-                leftBottomY,
-            )
-        }
-
-        fun pixelSize(width: Float, height: Float): FloatArray {
-            return floatArrayOf(
-                leftTopX * width,
-                leftTopY * height,
-                rightTopX * width,
-                rightTopY * height,
-                rightBottomX * width,
-                rightBottomY * height,
-                leftBottomX * width,
-                leftBottomY * height,
-            )
-        }
-
-        fun copyFrom(info: Radius) {
-            this.leftTopX = info.leftTopX
-            this.leftTopY = info.leftTopY
-            this.rightTopX = info.rightTopX
-            this.rightTopY = info.rightTopY
-            this.rightBottomX = info.rightBottomX
-            this.rightBottomY = info.rightBottomY
-            this.leftBottomX = info.leftBottomX
-            this.leftBottomY = info.leftBottomY
-        }
-
-        fun isSame(info: Radius): Boolean {
-            return (this.leftTopX == info.leftTopX &&
-                    this.leftTopY == info.leftTopY &&
-                    this.rightTopX == info.rightTopX &&
-                    this.rightTopY == info.rightTopY &&
-                    this.rightBottomX == info.rightBottomX &&
-                    this.rightBottomY == info.rightBottomY &&
-                    this.leftBottomX == info.leftBottomX &&
-                    this.leftBottomY == info.leftBottomY
                     )
         }
 
