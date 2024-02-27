af3bfaf2e284f25bdafb882504feedc3d5338a74
lollipop <liujian594@hotmail.com>
2024-02-27 21:30:52 +0800

    feat: OptionMenu还是需要手动写比较靠谱

--- app/src/main/java/com/lollipop/techo/dialog/BaseBottomSheetDialog.kt
+++ app/src/main/java/com/lollipop/techo/dialog/BaseBottomSheetDialog.kt
@@ -3,13 +3,27 @@ package com.lollipop.techo.dialog
 import android.content.Context
 import android.view.View
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.lollipop.pigment.Pigment
+import com.lollipop.pigment.PigmentPage
 import com.lollipop.techo.R
+import com.lollipop.techo.data.AppTheme
 
 open class BaseBottomSheetDialog(
     context: Context
-) : BottomSheetDialog(context, R.style.Theme_Techo_BottomSheetDialog) {
+) : BottomSheetDialog(context, R.style.Theme_Techo_BottomSheetDialog), PigmentPage {
 
     protected open val hideBackground = true
+
+    private var dialogPigment: Pigment? = null
+
+    override val currentPigment: Pigment?
+        get() {
+            return dialogPigment
+        }
+
+    fun setPigment(pigment: Pigment) {
+        this.dialogPigment = pigment
+    }
 
     override fun onStart() {
         super.onStart()
@@ -21,6 +35,25 @@ open class BaseBottomSheetDialog(
                 }
             }
         }
+        updatePigment()
+    }
+
+    private fun updatePigment() {
+        var pigment = currentPigment
+        if (pigment == null) {
+            val c = context
+            if (c is PigmentPage) {
+                pigment = c.currentPigment
+            }
+        }
+        if (pigment == null) {
+            pigment = AppTheme.current
+        }
+        onDecorationChanged(pigment)
+    }
+
+    override fun onDecorationChanged(pigment: Pigment) {
+        dialogPigment = pigment
     }
 
 }
---
+++ app/src/main/java/com/lollipop/techo/dialog/OptionMenuDialog.kt
@@ -0,0 +1,11 @@
+package com.lollipop.techo.dialog
+
+import android.content.Context
+
+/**
+ * OptionMenu默认上下文选择菜单现在用的是系统自带的，不方便切换样式
+ * 还是自己写的好控制一些
+ */
+class OptionMenuDialog(context: Context) : BaseBottomSheetDialog(context) {
+    // TODO 需要手动解析Menu信息
+}
