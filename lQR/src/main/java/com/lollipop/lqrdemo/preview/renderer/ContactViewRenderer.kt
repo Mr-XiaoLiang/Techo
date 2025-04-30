cbfaea9a8726dbf5ecf514980562146171fd8f8f
lollipop <liujian594@hotmail.com>
2025-04-30 21:02:55 +0800

    feat: 下一步是练习方式的渲染器，身体不舒服，慢点写吧

---
+++ lQR/src/main/java/com/lollipop/lqrdemo/preview/renderer/ContactViewRenderer.kt
@@ -0,0 +1,21 @@
+package com.lollipop.lqrdemo.preview.renderer
+
+import android.view.View
+import android.view.ViewGroup
+import com.lollipop.lqrdemo.preview.BarcodePreviewRenderer
+import com.lollipop.pigment.Pigment
+import com.lollipop.qr.comm.BarcodeWrapper
+
+class ContactViewRenderer: BarcodePreviewRenderer {
+    override fun getView(container: ViewGroup): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun render(barcode: BarcodeWrapper) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDecorationChanged(pigment: Pigment) {
+        TODO("Not yet implemented")
+    }
+}
