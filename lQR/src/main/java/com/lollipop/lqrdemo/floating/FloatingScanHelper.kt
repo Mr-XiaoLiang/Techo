87f9875134b870676e08f64652e2f4ef433bc4a1
lollipop <liujian594@hotmail.com>
2025-05-10 22:01:24 +0800

    feat: mediaProjection 不好搞

--- lQR/src/main/java/com/lollipop/lqrdemo/floating/FloatingScanHelper.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/floating/FloatingScanHelper.kt
@@ -1,13 +1,24 @@
 package com.lollipop.lqrdemo.floating
 
 import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.core.net.toUri
 
 object FloatingScanHelper {
 
+    private fun requestFloatingPermission(context: Context) {
+        // 引导用户去设置页面开启权限
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+        intent.data = "package:${context.packageName}".toUri()
+        context.startActivity(intent)
+    }
+
+    private fun checkFloatingPermission(context: Context): Boolean {
+        return Settings.canDrawOverlays(context);
+    }
+
     fun start(context: Context) {
-        if (!FloatingPermissionActivity.check(context)) {
-            return
-        }
         // TODO 启动扫描
     }
 
