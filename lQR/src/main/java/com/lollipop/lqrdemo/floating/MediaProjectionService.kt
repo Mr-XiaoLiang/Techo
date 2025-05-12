665100fa0b3e50cca39dbe03bf5e55777b15d4a0
lollipop <liujian594@hotmail.com>
2025-05-12 22:13:30 +0800

    feat: 没看明白图形解析的逻辑，后面再梳理一下

--- lQR/src/main/java/com/lollipop/lqrdemo/floating/MediaProjectionService.kt
+++ lQR/src/main/java/com/lollipop/lqrdemo/floating/MediaProjectionService.kt
@@ -1,12 +1,24 @@
 package com.lollipop.lqrdemo.floating
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.graphics.PixelFormat
+import android.hardware.display.VirtualDisplay
+import android.media.ImageReader
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
+import android.os.Build
 import android.os.IBinder
+import android.util.DisplayMetrics
 import android.util.Log
+import android.view.Display
+import android.view.WindowManager
+import com.lollipop.lqrdemo.R
 
 class MediaProjectionService : Service() {
 
@@ -14,6 +26,9 @@ class MediaProjectionService : Service() {
 
         private const val PARAMS_RESULT_KEY = "ResultKey"
         private const val ACTION_SCREENSHOT = "ActionScreenshot"
+
+        private const val CHANNEL_ID_MEDIA_PROJECTION = "CHANNEL_ID_MEDIA_PROJECTION"
+        private const val NOTIFICATION_ID_MEDIA_PROJECTION = 2333
 
         fun start(context: Context, resultKey: Int) {
             context.startForegroundService(
@@ -24,12 +39,13 @@ class MediaProjectionService : Service() {
         }
 
         private fun createScreenshotDelegate(
+            context: Context,
             manager: MediaProjectionManager?,
             mpResult: MediaProjectionHelper.MPResult
         ): ScreenshotDelegate? {
             manager ?: return null
             val mp = mpResult.getMediaProjection(manager) ?: return null
-            return ScreenshotDelegate(mp)
+            return ScreenshotDelegate(context, mp)
         }
 
     }
@@ -54,7 +70,9 @@ class MediaProjectionService : Service() {
             if (mpResult != null) {
                 screenshotDelegate?.release()
                 screenshotDelegate = createScreenshotDelegate(
-                    mediaProjectionManager, mpResult
+                    this,
+                    mediaProjectionManager,
+                    mpResult
                 )
             }
         }
@@ -69,7 +87,29 @@ class MediaProjectionService : Service() {
     }
 
     private fun sendForegroundNotification() {
-        // TODO
+        val notificationManager = getSystemService(NotificationManager::class.java) ?: return
+        val channel = NotificationChannel(
+            CHANNEL_ID_MEDIA_PROJECTION,
+            getString(R.string.notification_channel_scan),
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        val notificationBuilder = Notification.Builder(this, CHANNEL_ID_MEDIA_PROJECTION)
+            .setSmallIcon(R.drawable.ic_lqr_24dp)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.notification_content_scan))
+
+        val notification = notificationBuilder.build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(
+                NOTIFICATION_ID_MEDIA_PROJECTION,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION
+            )
+        } else {
+            startForeground(NOTIFICATION_ID_MEDIA_PROJECTION, notification)
+        }
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -82,18 +122,84 @@ class MediaProjectionService : Service() {
     }
 
     private class ScreenshotDelegate(
+        val context: Context,
         val mediaProjection: MediaProjection
-    ) {
+    ) : MediaProjection.Callback() {
+
+        private var imageReader: ImageReader? = null
+        private var virtualDisplayImageReader: VirtualDisplay? = null
+
+        var isAvailable = false
+            private set
 
         init {
             createImageReaderVirtualDisplay()
         }
 
+        private fun getDisplayMetrics(): DisplayMetrics? {
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as? WindowManager
+            if (windowManager == null) {
+                return null
+            }
+            val displayMetrics = DisplayMetrics()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val windowMetrics = windowManager.currentWindowMetrics
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    displayMetrics.densityDpi = windowMetrics.density.toInt()
+                } else {
+                    context.display.getRealMetrics(displayMetrics)
+                }
+                displayMetrics.widthPixels = windowMetrics.bounds.width()
+                displayMetrics.heightPixels = windowMetrics.bounds.height()
+            } else {
+                windowManager.defaultDisplay.getRealMetrics(displayMetrics)
+            }
+            return displayMetrics
+        }
+
         private fun createImageReaderVirtualDisplay() {
+            val displayMetrics = getDisplayMetrics() ?: return
+            val reader = ImageReader.newInstance(
+                displayMetrics.widthPixels, displayMetrics.heightPixels, PixelFormat.RGBA_8888, 1
+            )
+            imageReader = reader
+            reader.setOnImageAvailableListener(
+                ImageReader.OnImageAvailableListener { reader: ImageReader? ->
+                    isAvailable = true
+                },
+                null
+            )
+            mediaProjection.registerCallback(this, null)
+            virtualDisplayImageReader =
+                mediaProjection.createVirtualDisplay(
+                    "ImageReader",
+                    displayMetrics.widthPixels,
+                    displayMetrics.heightPixels,
+                    displayMetrics.densityDpi,
+                    Display.FLAG_ROUND,
+                    reader.surface,
+                    null,
+                    null
+                )
             // TODO
         }
 
         fun release() {
+            // TODO
+        }
+
+        override fun onCapturedContentResize(width: Int, height: Int) {
+            super.onCapturedContentResize(width, height)
+            // TODO
+        }
+
+        override fun onCapturedContentVisibilityChanged(isVisible: Boolean) {
+            super.onCapturedContentVisibilityChanged(isVisible)
+            // TODO
+        }
+
+        override fun onStop() {
+            super.onStop()
             // TODO
         }
 
